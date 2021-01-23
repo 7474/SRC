b25956364415e4067a6ac0f166966661f2570947
koudenpa <koudenpa@hotmail.com>
2021-01-24 00:28:27 +0900

    メインのファイル名。

--- SRC.Sharp/SRCTestForm/frmMain.Designer.cs
+++ SRC.Sharp/SRCTestForm/frmMain.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace SRCTestForm
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,10 +29,17 @@ namespace SRCTestForm
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "frmMain";
+            this.Text = "SRCTestForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
