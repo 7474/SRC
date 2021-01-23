4ce8debf2fc4ea5bb6849b3a72e028b958300ee4
koudenpa <koudenpa@hotmail.com>
2021-01-24 05:18:44 +0900

    SkillData持ってきた。

---
+++ SRC.Sharp/SRCCore/Models/SkillData.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Project1
+{
+    internal class SkillData
+    {
+
+        // Copyright (C) 1997-2012 Kei Sakamoto / Inui Tetsuyuki
+        // 本プログラムはフリーソフトであり、無保証です。
+        // 本プログラムはGNU General Public License(Ver.3またはそれ以降)が定める条件の下で
+        // 再頒布または改変することができます。
+
+        // パイロット用特殊能力のクラス
+
+        // 名称
+        public string Name;
+        // レベル (レベル指定のない能力の場合はDEFAULT_LEVEL)
+        public double Level;
+        // データ
+        public string StrData;
+        // 習得レベル
+        public short NecessaryLevel;
+    }
+}
