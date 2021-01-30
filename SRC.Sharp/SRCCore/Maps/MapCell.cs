df903a625ca46e8e4eda8897a33fdec8a184d370
koudenpa <koudenpa@hotmail.com>
2021-01-31 01:53:47 +0900

    コメントアウト

---
+++ SRC.Sharp/SRCCore/Maps/MapCell.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SRC.Core.Maps
+{
+    public class MapCell
+    {
+        // XXX Enumにするとか
+        public int TerrainType { get; set; }
+        public int BitmapNo { get; set; }
+        public int LayerType { get; set; }
+        public int LayerBitmapNo { get; set; }
+        public int BoxType { get; set; }
+    }
+}
