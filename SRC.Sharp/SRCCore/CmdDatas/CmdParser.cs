fbc59c5bbc28ec8820b580595872999275ff6843
koudenpa <koudenpa@hotmail.com>
2021-01-31 06:52:18 +0900

    パーサというかファクトリというか、コマンドは多態をとりたい。

---
+++ SRC.Sharp/SRCCore/CmdDatas/CmdParser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SRC.Core.CmdDatas
+{
+    public class CmdParser
+    {
+        public CmdData Parse(string edata)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
