06edf0ab5a9ed32f849b5a04a93d286433e58b41
koudenpa <koudenpa@hotmail.com>
2021-02-01 02:14:39 +0900

    Formの読む場所変えた。

--- SRC.Sharp/SRCTestForm/frmMain.gui.cs
+++ SRC.Sharp/SRCTestForm/frmMain.gui.cs
@@ -35,8 +35,165 @@ namespace SRCTestForm
         {
             Console.WriteLine("LoadForms");
 
-            frmNowLoading = new frmNowLoading();
-            frmNowLoading.Hide();
+            //short X, Y;
+
+            //// UPGRADE_ISSUE: Load ステートメント はサポートされていません。 詳細については、'ms-help://MS.VSCC.v90/dv_commoner/local/redirect.htm?keyword="B530EFF2-3132-48F8-B8BC-D88AF543D321"' をクリックしてください。
+            //Load(My.MyProject.Forms.frmToolTip);
+            //// UPGRADE_ISSUE: Load ステートメント はサポートされていません。 詳細については、'ms-help://MS.VSCC.v90/dv_commoner/local/redirect.htm?keyword="B530EFF2-3132-48F8-B8BC-D88AF543D321"' をクリックしてください。
+            //Load(My.MyProject.Forms.frmMessage);
+            //// UPGRADE_ISSUE: Load ステートメント はサポートされていません。 詳細については、'ms-help://MS.VSCC.v90/dv_commoner/local/redirect.htm?keyword="B530EFF2-3132-48F8-B8BC-D88AF543D321"' をクリックしてください。
+            //Load(My.MyProject.Forms.frmListBox);
+            //LockGUI();
+            //Commands.CommandState = "ユニット選択";
+
+            //// マップ画面に表示できるマップのサイズ
+            //string argini_section1 = "Option";
+            //string argini_entry1 = "NewGUI";
+            //switch (Strings.LCase(GeneralLib.ReadIni(ref argini_section1, ref argini_entry1)) ?? "")
+            //{
+            //    case "on":
+            //        {
+            //            // MOD START MARGE
+            //            NewGUIMode = true;
+            //            // MOD END MARGE
+            //            MainWidth = 20;
+            //            break;
+            //        }
+
+            //    case "off":
+            //        {
+            //            MainWidth = 15;
+            //            break;
+            //        }
+
+            //    default:
+            //        {
+            //            MainWidth = 15;
+            //            string argini_section = "Option";
+            //            string argini_entry = "NewGUI";
+            //            string argini_data = "Off";
+            //            GeneralLib.WriteIni(ref argini_section, ref argini_entry, ref argini_data);
+            //            break;
+            //        }
+            //}
+            //// ADD START MARGE
+            //// Optionで定義されていればそちらを優先する
+            //string argoname = "新ＧＵＩ";
+            //if (Expression.IsOptionDefined(ref argoname))
+            //{
+            //    NewGUIMode = true;
+            //    MainWidth = 20;
+            //}
+            //// ADD END MARGE
+            //MainHeight = 15;
+
+            //// マップ画面のサイズ（ピクセル）
+            //MainPWidth = (short)(MainWidth * 32);
+            //MainPHeight = (short)(MainHeight * 32);
+            //{
+            //    var withBlock = MainForm;
+            //    // メインウィンドウの位置＆サイズを設定
+            //    X = (short)Microsoft.VisualBasic.Compatibility.VB6.Support.TwipsPerPixelX();
+            //    Y = (short)Microsoft.VisualBasic.Compatibility.VB6.Support.TwipsPerPixelY();
+            //    // MOD START MARGE
+            //    // If MainWidth = 15 Then
+            //    if (!NewGUIMode)
+            //    {
+            //        // MOD END MARGE
+            //        withBlock.Width = (int)Microsoft.VisualBasic.Compatibility.VB6.Support.TwipsToPixelsX(Microsoft.VisualBasic.Compatibility.VB6.Support.PixelsToTwipsX(withBlock.Width) - Microsoft.VisualBasic.Compatibility.VB6.Support.PixelsToTwipsX(withBlock.ClientRectangle.Width) * X + (MainPWidth + 24 + 225 + 4) * X);
+            //        withBlock.Height = (int)Microsoft.VisualBasic.Compatibility.VB6.Support.TwipsToPixelsY(Microsoft.VisualBasic.Compatibility.VB6.Support.PixelsToTwipsY(withBlock.Height) - Microsoft.VisualBasic.Compatibility.VB6.Support.PixelsToTwipsY(withBlock.ClientRectangle.Height) * Y + (MainPHeight + 24) * Y);
+            //    }
+            //    else
+            //    {
+            //        withBlock.Width = (int)Microsoft.VisualBasic.Compatibility.VB6.Support.TwipsToPixelsX(Microsoft.VisualBasic.Compatibility.VB6.Support.PixelsToTwipsX(withBlock.Width) - Microsoft.VisualBasic.Compatibility.VB6.Support.PixelsToTwipsX(withBlock.ClientRectangle.Width) * X + MainPWidth * X);
+            //        withBlock.Height = (int)Microsoft.VisualBasic.Compatibility.VB6.Support.TwipsToPixelsY(Microsoft.VisualBasic.Compatibility.VB6.Support.PixelsToTwipsY(withBlock.Height) - Microsoft.VisualBasic.Compatibility.VB6.Support.PixelsToTwipsY(withBlock.ClientRectangle.Height) * Y + MainPHeight * Y);
+            //    }
+
+            //    withBlock.Left = (int)Microsoft.VisualBasic.Compatibility.VB6.Support.TwipsToPixelsX((Microsoft.VisualBasic.Compatibility.VB6.Support.PixelsToTwipsX(Screen.PrimaryScreen.Bounds.Width) - Microsoft.VisualBasic.Compatibility.VB6.Support.PixelsToTwipsX(withBlock.Width)) / 2d);
+            //    withBlock.Top = (int)Microsoft.VisualBasic.Compatibility.VB6.Support.TwipsToPixelsY((Microsoft.VisualBasic.Compatibility.VB6.Support.PixelsToTwipsY(Screen.PrimaryScreen.Bounds.Height) - Microsoft.VisualBasic.Compatibility.VB6.Support.PixelsToTwipsY(withBlock.Height)) / 2d);
+
+            //    // スクロールバーの位置を設定
+            //    // MOD START MARGE
+            //    // If MainWidth = 15 Then
+            //    if (!NewGUIMode)
+            //    {
+            //        // MOD END MARGE
+            //        // UPGRADE_ISSUE: Control VScroll は、汎用名前空間 Form 内にあるため、解決できませんでした。 詳細については、'ms-help://MS.VSCC.v90/dv_commoner/local/redirect.htm?keyword="084D22AD-ECB1-400F-B4C7-418ECEC5E36E"' をクリックしてください。
+            //        withBlock.VScroll.Move(MainPWidth + 4, 4, 16, MainPWidth);
+            //        // UPGRADE_ISSUE: Control HScroll は、汎用名前空間 Form 内にあるため、解決できませんでした。 詳細については、'ms-help://MS.VSCC.v90/dv_commoner/local/redirect.htm?keyword="084D22AD-ECB1-400F-B4C7-418ECEC5E36E"' をクリックしてください。
+            //        withBlock.HScroll.Move(4, MainPHeight + 4, MainPWidth, 16);
+            //    }
+            //    else
+            //    {
+            //        // UPGRADE_ISSUE: Control VScroll は、汎用名前空間 Form 内にあるため、解決できませんでした。 詳細については、'ms-help://MS.VSCC.v90/dv_commoner/local/redirect.htm?keyword="084D22AD-ECB1-400F-B4C7-418ECEC5E36E"' をクリックしてください。
+            //        withBlock.VScroll.Visible = false;
+            //        // UPGRADE_ISSUE: Control HScroll は、汎用名前空間 Form 内にあるため、解決できませんでした。 詳細については、'ms-help://MS.VSCC.v90/dv_commoner/local/redirect.htm?keyword="084D22AD-ECB1-400F-B4C7-418ECEC5E36E"' をクリックしてください。
+            //        withBlock.HScroll.Visible = false;
+            //    }
+
+            //    // ステータスウィンドウを設置
+            //    // MOD START MARGE
+            //    // If MainWidth = 15 Then
+            //    // .picFace.Move MainPWidth + 24, 4
+            //    // .picPilotStatus.Move MainPWidth + 24 + 68 + 4, 4, 155, 72
+            //    // .picUnitStatus.Move MainPWidth + 24, 4 + 68 + 4, _
+            //    // '                225 + 5, MainPHeight - 64 + 16
+            //    // Else
+            //    // .picUnitStatus.Move MainPWidth - 230 - 10, 10, 230, MainPHeight - 20
+            //    // .picUnitStatus.Visible = False
+            //    // .picPilotStatus.Visible = False
+            //    // .picFace.Visible = False
+            //    // End If
+            //    if (NewGUIMode)
+            //    {
+            //        // UPGRADE_ISSUE: Control picUnitStatus は、汎用名前空間 Form 内にあるため、解決できませんでした。 詳細については、'ms-help://MS.VSCC.v90/dv_commoner/local/redirect.htm?keyword="084D22AD-ECB1-400F-B4C7-418ECEC5E36E"' をクリックしてください。
+            //        withBlock.picUnitStatus.Move(MainPWidth - 230 - 10, 10, 230, MainPHeight - 20);
+            //        // UPGRADE_ISSUE: Control picUnitStatus は、汎用名前空間 Form 内にあるため、解決できませんでした。 詳細については、'ms-help://MS.VSCC.v90/dv_commoner/local/redirect.htm?keyword="084D22AD-ECB1-400F-B4C7-418ECEC5E36E"' をクリックしてください。
+            //        withBlock.picUnitStatus.Visible = false;
+            //        // UPGRADE_ISSUE: Control picPilotStatus は、汎用名前空間 Form 内にあるため、解決できませんでした。 詳細については、'ms-help://MS.VSCC.v90/dv_commoner/local/redirect.htm?keyword="084D22AD-ECB1-400F-B4C7-418ECEC5E36E"' をクリックしてください。
+            //        withBlock.picPilotStatus.Visible = false;
+            //        // UPGRADE_ISSUE: Control picFace は、汎用名前空間 Form 内にあるため、解決できませんでした。 詳細については、'ms-help://MS.VSCC.v90/dv_commoner/local/redirect.htm?keyword="084D22AD-ECB1-400F-B4C7-418ECEC5E36E"' をクリックしてください。
+            //        withBlock.picFace.Visible = false;
+            //        Status.StatusWindowBackBolor = STATUSBACK;
+            //        Status.StatusWindowFrameColor = STATUSBACK;
+            //        Status.StatusWindowFrameWidth = 1;
+            //        // UPGRADE_ISSUE: Control picUnitStatus は、汎用名前空間 Form 内にあるため、解決できませんでした。 詳細については、'ms-help://MS.VSCC.v90/dv_commoner/local/redirect.htm?keyword="084D22AD-ECB1-400F-B4C7-418ECEC5E36E"' をクリックしてください。
+            //        withBlock.picUnitStatus.BackColor = Status.StatusWindowBackBolor;
+            //        Status.StatusFontColorAbilityName = Information.RGB(0, 0, 150);
+            //        Status.StatusFontColorAbilityEnable = ColorTranslator.ToOle(Color.Blue);
+            //        Status.StatusFontColorAbilityDisable = Information.RGB(150, 0, 0);
+            //        Status.StatusFontColorNormalString = ColorTranslator.ToOle(Color.Black);
+            //    }
+            //    else
+            //    {
+            //        // UPGRADE_ISSUE: Control picFace は、汎用名前空間 Form 内にあるため、解決できませんでした。 詳細については、'ms-help://MS.VSCC.v90/dv_commoner/local/redirect.htm?keyword="084D22AD-ECB1-400F-B4C7-418ECEC5E36E"' をクリックしてください。
+            //        withBlock.picFace.Move(MainPWidth + 24, 4);
+            //        // UPGRADE_ISSUE: Control picPilotStatus は、汎用名前空間 Form 内にあるため、解決できませんでした。 詳細については、'ms-help://MS.VSCC.v90/dv_commoner/local/redirect.htm?keyword="084D22AD-ECB1-400F-B4C7-418ECEC5E36E"' をクリックしてください。
+            //        withBlock.picPilotStatus.Move(MainPWidth + 24 + 68 + 4, 4, 155, 72);
+            //        // UPGRADE_ISSUE: Control picUnitStatus は、汎用名前空間 Form 内にあるため、解決できませんでした。 詳細については、'ms-help://MS.VSCC.v90/dv_commoner/local/redirect.htm?keyword="084D22AD-ECB1-400F-B4C7-418ECEC5E36E"' をクリックしてください。
+            //        withBlock.picUnitStatus.Move(MainPWidth + 24, 4 + 68 + 4, 225 + 5, MainPHeight - 64 + 16);
+            //    }
+            //    // MOD END MARGE
+
+            //    // マップウィンドウのサイズを設定
+            //    // MOD START MARGE
+            //    // If MainWidth = 15 Then
+            //    if (!NewGUIMode)
+            //    {
+            //        // MOD END MARGE
+            //        // UPGRADE_ISSUE: Control picMain は、汎用名前空間 Form 内にあるため、解決できませんでした。 詳細については、'ms-help://MS.VSCC.v90/dv_commoner/local/redirect.htm?keyword="084D22AD-ECB1-400F-B4C7-418ECEC5E36E"' をクリックしてください。
+            //        withBlock.picMain(0).Move(4, 4, MainPWidth, MainPHeight);
+            //        // UPGRADE_ISSUE: Control picMain は、汎用名前空間 Form 内にあるため、解決できませんでした。 詳細については、'ms-help://MS.VSCC.v90/dv_commoner/local/redirect.htm?keyword="084D22AD-ECB1-400F-B4C7-418ECEC5E36E"' をクリックしてください。
+            //        withBlock.picMain(1).Move(4, 4, MainPWidth, MainPHeight);
+            //    }
+            //    else
+            //    {
+            //        // UPGRADE_ISSUE: Control picMain は、汎用名前空間 Form 内にあるため、解決できませんでした。 詳細については、'ms-help://MS.VSCC.v90/dv_commoner/local/redirect.htm?keyword="084D22AD-ECB1-400F-B4C7-418ECEC5E36E"' をクリックしてください。
+            //        withBlock.picMain(0).Move(0, 0, MainPWidth, MainPHeight);
+            //        // UPGRADE_ISSUE: Control picMain は、汎用名前空間 Form 内にあるため、解決できませんでした。 詳細については、'ms-help://MS.VSCC.v90/dv_commoner/local/redirect.htm?keyword="084D22AD-ECB1-400F-B4C7-418ECEC5E36E"' をクリックしてください。
+            //        withBlock.picMain(1).Move(0, 0, MainPWidth, MainPHeight);
+            //    }
+            //}
         }
 
         void IGUI.SetNewGUIMode()
@@ -304,12 +461,15 @@ namespace SRCTestForm
 
         void IGUI.OpenNowLoadingForm()
         {
+            frmNowLoading = new frmNowLoading();
             frmNowLoading.Show(this);
         }
 
         void IGUI.CloseNowLoadingForm()
         {
-            frmNowLoading.Hide();
+            frmNowLoading.Close();
+            frmNowLoading.Dispose();
+            frmNowLoading = null;
         }
 
         void IGUI.DisplayLoadingProgress()
