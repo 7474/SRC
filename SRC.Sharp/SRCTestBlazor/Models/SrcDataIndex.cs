eff12c06b36abef3e4c9cff7052cc998833857f4
koudenpa <koudenpa@hotmail.com>
2021-02-03 04:57:58 +0900

    SRC-DATAに置いたIndexを読む。

---
+++ SRC.Sharp/SRCTestBlazor/Models/SrcDataIndex.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SRCTestBlazor.Models
+{
+    public class SrcDataIndex
+    {
+        public ICollection<TitleData> Titles { get; set; }
+    }
+    public class TitleData
+    {
+        public string Title { get; set; }
+        public string Base { get; set; }
+        public string Path { get; set; }
+        public ICollection<string> Tags { get; set; }
+    }
+}
