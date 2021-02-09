26d492e1d963799ecb149e756f8e76a2d9f74e40
koudenpa <koudenpa@hotmail.com>
2021-02-10 00:11:45 +0900

    ざっくりブルマ。

--- SRC.Sharp/SRCTestBlazor/Program.cs
+++ SRC.Sharp/SRCTestBlazor/Program.cs
@@ -1,14 +1,7 @@
-using Blazorise;
-using Blazorise.Bulma;
-using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace SRCTestBlazor
@@ -23,23 +16,11 @@ namespace SRCTestBlazor
                 .AddScoped(sp => new HttpClient
                 {
                     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
-                })
-                .AddBlazorise(options =>
-                {
-                    // XXXX ñ{ìñÇ…ÅH
-                    // https://blazorise.com/docs/usage/bulma/
-                    options.ChangeTextOnKeyPress = true;
-                })
-                .AddBulmaProviders()
-                .AddFontAwesomeIcons();
+                });
 
             builder.RootComponents.Add<App>("#app");
 
             var host = builder.Build();
-
-            host.Services.UseBulmaProviders()
-                .UseFontAwesomeIcons();
-
             await host.RunAsync();
         }
     }
