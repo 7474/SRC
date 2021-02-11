3d2bc0af8d5bbcfa42e572933a172c841736e339
koudenpa <koudenpa@hotmail.com>
2021-02-12 07:55:36 +0900

    メッセージを簡易にレンダリング。

---
+++ SRC.Sharp/SRCTestBlazor/Models/MessagePreviewData.cs
@@ -0,0 +1,22 @@
+﻿using Markdig;
+using Microsoft.AspNetCore.Components;
+
+namespace SRCTestBlazor.Models
+{
+    public class MessagePreviewData
+    {
+        public string BitmapUri { get; }
+        public MarkupString Message { get; }
+
+        public MessagePreviewData(string bitmapUri, string message)
+        {
+            BitmapUri = bitmapUri;
+            Message = new MarkupString(Markdown.ToHtml(
+                message
+                    .Replace(";", "<br>")
+                    .Replace(".", "<br>")
+                    .Replace(":", "")
+                ));
+        }
+    }
+}
