d978df2a2981e0e06b8ef99281b2a85d05de1f82
koudenpa <koudenpa@hotmail.com>
2021-02-12 05:50:32 +0900

    MessageDataList#Load

--- SRC.Sharp/SRCCore/SRC.data.cs
+++ SRC.Sharp/SRCCore/SRC.data.cs
@@ -87,12 +87,11 @@ namespace SRCCore
             //    GUI.DisplayLoadingProgress();
             //}
 
-            //string argfname14 = fpath + @"\pilot_message.txt";
-            //if (GeneralLib.FileExists(ref argfname14))
-            //{
-            //    string argfname13 = fpath + @"\pilot_message.txt";
-            //    MDList.Load(ref argfname13);
-            //}
+            var pilotMessageFilePath = Path.Combine(fpath, "pilot_message.txt");
+            if (GeneralLib.FileExists(pilotMessageFilePath))
+            {
+                MDList.Load(pilotMessageFilePath, false);
+            }
 
             //string argfname16 = fpath + @"\pilot_dialog.txt";
             //if (GeneralLib.FileExists(ref argfname16))
