859c5cd444f34aeba40f0f8ae72a852d1f9d2c5a
koudenpa <koudenpa@hotmail.com>
2021-03-05 04:04:40 +0900

    ざっとIfを事前処理。

--- SRC.Sharp/SRCCore/CmdDatas/Commands/AIfCmd.cs
+++ SRC.Sharp/SRCCore/CmdDatas/Commands/AIfCmd.cs
@@ -1,11 +1,14 @@
 ﻿using SRCCore.Events;
 using SRCCore.Exceptions;
+using SRCCore.VB;
 using System;
+using System.Collections.Generic;
 
 namespace SRCCore.CmdDatas.Commands
 {
     public enum IfCmdType
     {
+        Undefined,
         Then,
         Exit,
         GoTo,
@@ -14,6 +17,10 @@ namespace SRCCore.CmdDatas.Commands
     public abstract class AIfCmd : CmdData
     {
         public IfCmdType IfCmdType { get; protected set; }
+        protected string Expr { get; set; }
+        // Exprの項の数、0なら単に式として評価するべきExpr。
+        protected int ExprTermCount { get; set; }
+        protected string GoToLabel { get; set; }
 
         public AIfCmd(SRC src, CmdType name, EventDataLine eventData) : base(src, name, eventData)
         {
@@ -21,157 +28,100 @@ namespace SRCCore.CmdDatas.Commands
 
         protected void PrepareArgs()
         {
-            // TODO Impl
-            //if (CmdName == CmdType.IfCmd | CmdName == CmdType.ElseIfCmd)
-            //{
-            //    // If文の処理の高速化のため、あらかじめ構文解析しておく
-            //    if (ArgNum == 1)
-            //    {
-            //        // 書式エラー
-            //        Event_Renamed.DisplayEventErrorMessage(Event_Renamed.CurrentLineNum, "Ifコマンドの書式に合っていません");
-            //        ParseRet = false;
-            //        return ParseRet;
-            //    }
+            // If文の処理の高速化のため、あらかじめ構文解析しておく
+            if (ArgNum == 1)
+            {
+                // 書式エラー
+                throw new EventErrorException(this, "Ifコマンドの書式に合っていません");
+            }
 
-            //    expr = list[2];
-            //    var loopTo2 = ArgNum;
-            //    for (i = 3; i <= loopTo2; i++)
-            //    {
-            //        buf = list[i];
-            //        switch (Strings.LCase(buf) ?? "")
-            //        {
-            //            case "then":
-            //            case "exit":
-            //                {
-            //                    // UPGRADE_WARNING: 配列 strArgs の下限が 2 から 0 に変更されました。 詳細については、'ms-help://MS.VSCC.v90/dv_commoner/local/redirect.htm?keyword="0F1C9BE1-AF9D-476E-83B1-17D43BECFF20"' をクリックしてください。
-            //                    strArgs = new string[5];
-            //                    // UPGRADE_WARNING: 配列 lngArgs の下限が 2 から 0 に変更されました。 詳細については、'ms-help://MS.VSCC.v90/dv_commoner/local/redirect.htm?keyword="0F1C9BE1-AF9D-476E-83B1-17D43BECFF20"' をクリックしてください。
-            //                    lngArgs = new int[5];
-            //                    // UPGRADE_WARNING: 配列 dblArgs の下限が 2 から 0 に変更されました。 詳細については、'ms-help://MS.VSCC.v90/dv_commoner/local/redirect.htm?keyword="0F1C9BE1-AF9D-476E-83B1-17D43BECFF20"' をクリックしてください。
-            //                    dblArgs = new double[5];
-            //                    // UPGRADE_WARNING: 配列 ArgsType の下限が 2 から 0 に変更されました。 詳細については、'ms-help://MS.VSCC.v90/dv_commoner/local/redirect.htm?keyword="0F1C9BE1-AF9D-476E-83B1-17D43BECFF20"' をクリックしてください。
-            //                    ArgsType = new Expressions.ValueType[5];
-            //                    strArgs[2] = expr;
-            //                    lngArgs[3] = ArgNum - 2;
-            //                    ArgsType[3] = Expressions.ValueType.NumericType;
-            //                    strArgs[4] = Strings.LCase(buf);
-            //                    break;
-            //                }
+            var ifCmdType = IfCmdType.Undefined;
+            var terms = new List<string>();
+            terms.Add(GetArg(2));
+            for (var i = 3; i <= ArgNum; i++)
+            {
+                var buf = GetArg(i);
+                switch (Strings.LCase(buf) ?? "")
+                {
+                    case "then":
+                        ifCmdType = IfCmdType.Then;
+                        break;
+                    case "exit":
+                        ifCmdType = IfCmdType.Exit;
+                        break;
 
-            //            case "goto":
-            //                {
-            //                    buf = GetArg((int)(i + 1));
-            //                    // UPGRADE_WARNING: 配列 strArgs の下限が 2 から 0 に変更されました。 詳細については、'ms-help://MS.VSCC.v90/dv_commoner/local/redirect.htm?keyword="0F1C9BE1-AF9D-476E-83B1-17D43BECFF20"' をクリックしてください。
-            //                    strArgs = new string[6];
-            //                    // UPGRADE_WARNING: 配列 lngArgs の下限が 2 から 0 に変更されました。 詳細については、'ms-help://MS.VSCC.v90/dv_commoner/local/redirect.htm?keyword="0F1C9BE1-AF9D-476E-83B1-17D43BECFF20"' をクリックしてください。
-            //                    lngArgs = new int[6];
-            //                    // UPGRADE_WARNING: 配列 dblArgs の下限が 2 から 0 に変更されました。 詳細については、'ms-help://MS.VSCC.v90/dv_commoner/local/redirect.htm?keyword="0F1C9BE1-AF9D-476E-83B1-17D43BECFF20"' をクリックしてください。
-            //                    dblArgs = new double[6];
-            //                    // UPGRADE_WARNING: 配列 ArgsType の下限が 2 から 0 に変更されました。 詳細については、'ms-help://MS.VSCC.v90/dv_commoner/local/redirect.htm?keyword="0F1C9BE1-AF9D-476E-83B1-17D43BECFF20"' をクリックしてください。
-            //                    ArgsType = new Expressions.ValueType[6];
-            //                    strArgs[2] = expr;
-            //                    lngArgs[3] = ArgNum - 3;
-            //                    ArgsType[3] = Expressions.ValueType.NumericType;
-            //                    strArgs[4] = "goto";
-            //                    strArgs[5] = buf;
-            //                    break;
-            //                }
+                    case "goto":
+                        ifCmdType = IfCmdType.GoTo;
+                        GoToLabel = GetArg(i + 1);
+                        break;
 
-            //            case var case1 when case1 == "":
-            //                {
-            //                    buf = "\"\"";
-            //                    break;
-            //                }
-            //        }
+                    default:
+                        break;
+                }
+                if (ifCmdType != IfCmdType.Undefined) { break; }
 
-            //        expr = expr + " " + buf;
-            //    }
+                terms.Add(string.IsNullOrEmpty(buf) ? "\"\"" : buf);
+            }
 
-            //    if (i > ArgNum)
-            //    {
-            //        if (CmdName == CmdType.IfCmd)
-            //        {
-            //            Event_Renamed.DisplayEventErrorMessage(LineNum, "Ifに対応する Then または Exit または Goto がありません");
-            //        }
-            //        else
-            //        {
-            //            Event_Renamed.DisplayEventErrorMessage(LineNum, "ElseIfに対応する Then または Exit または Goto がありません");
-            //        }
+            if (ifCmdType == IfCmdType.Undefined)
+            {
+                throw new EventErrorException(this, (Name == CmdType.IfCmd ? "If" : "ElseIf") + "に対応する Then または Exit または Goto がありません");
+            }
 
-            //        SRC.TerminateSRC();
-            //    }
+            // 条件式が式であることが確定していれば条件式の項数を0に
+            switch (terms.Count)
+            {
+                case 0:
+                    throw new EventErrorException(this, (Name == CmdType.IfCmd ? "If" : "ElseIf") + "コマンドの条件式がありません Then または Exit または Goto がありません");
 
-            //    // 条件式が式であることが確定していれば条件式の項数を0に
-            //    switch (lngArgs[3])
-            //    {
-            //        case 0:
-            //            {
-            //                if (CmdName == CmdType.IfCmd)
-            //                {
-            //                    Event_Renamed.DisplayEventErrorMessage(LineNum, "Ifコマンドの条件式がありません");
-            //                }
-            //                else
-            //                {
-            //                    Event_Renamed.DisplayEventErrorMessage(LineNum, "ElseIfコマンドの条件式がありません");
-            //                }
+                case 1:
+                    switch (Strings.Asc(terms[0]))
+                    {
+                        case 36: // $
+                            // 変数なら式
+                            ExprTermCount = 0;
+                            break;
 
-            //                SRC.TerminateSRC();
-            //                break;
-            //            }
+                        case 40: // (
+                            // カッコで囲われていたら式
+                            // ()を除去
+                            terms[0] = Strings.Mid(terms[0], 2, Strings.Len(terms[0]) - 2);
+                            ExprTermCount = 0;
+                            break;
+                        default:
+                            // そうでないならパイロット指定
+                            break;
+                    }
 
-            //        case 1:
-            //            {
-            //                switch (Strings.Asc(expr))
-            //                {
-            //                    case 36: // $
-            //                        {
-            //                            lngArgs[3] = 0;
-            //                            break;
-            //                        }
+                    break;
 
-            //                    case 40: // (
-            //                        {
-            //                            // ()を除去
-            //                            strArgs[2] = Strings.Mid(expr, 2, Strings.Len(expr) - 2);
-            //                            lngArgs[3] = 0;
-            //                            break;
-            //                        }
-            //                }
+                case 2:
+                    if (Strings.LCase(terms[0]) == "not")
+                    {
+                        switch (Strings.Asc(terms[1]))
+                        {
+                            case 36:
+                            case 40: // $, (
+                                // 変数かカッコで囲われていたら式
+                                ExprTermCount = 0;
+                                break;
+                            default:
+                                // そうでないならパイロット指定
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        // 1つ目の項がNotでないなら式
+                        ExprTermCount = 0;
+                    }
+                    break;
 
-            //                break;
-            //            }
-
-            //        case 2:
-            //            {
-            //                if (Strings.LCase(GeneralLib.LIndex(expr, 1)) == "not")
-            //                {
-            //                    switch (Strings.Asc(GeneralLib.ListIndex(expr, 2)))
-            //                    {
-            //                        case 36:
-            //                        case 40: // $, (
-            //                            {
-            //                                lngArgs[3] = 0;
-            //                                break;
-            //                            }
-            //                    }
-            //                }
-            //                else
-            //                {
-            //                    lngArgs[3] = 0;
-            //                }
-
-            //                break;
-            //            }
-
-            //        default:
-            //            {
-            //                lngArgs[3] = 0;
-            //                break;
-            //            }
-            //    }
-
-            //    return ParseRet;
-            //}
+                default:
+                    ExprTermCount = 0;
+                    break;
+            }
+            Expr = string.Join(" ", terms);
         }
 
         internal static int ToEnd(CmdData elseCmd)
