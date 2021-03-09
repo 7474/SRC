0a71c3d194caff5079bded47ad03c7ed2bbf7add
koudenpa <koudenpa@hotmail.com>
2021-03-10 00:56:34 +0900

    Loop

--- SRC.Sharp/SRCTestForm/Resoruces/WindowsManagedPlayer.cs
+++ SRC.Sharp/SRCTestForm/Resoruces/WindowsManagedPlayer.cs
@@ -92,15 +92,18 @@ namespace SRCTestForm.Resoruces
         {
             if (!outputMap.ContainsKey(channel))
             {
+                var outputDevice = new WaveOutEvent();
+                //outputDevice.PlaybackStopped += OnPlaybackStopped;
                 outputMap[channel] = new WaveChannel()
                 {
-                    outputDevice = new WaveOutEvent(),
+                    outputDevice = outputDevice,
                 };
-                //outputDevice.PlaybackStopped += OnPlaybackStopped;
             }
             WaveChannel waveChannel = outputMap[channel];
             waveChannel.repeat = mode.HasFlag(PlaySoundMode.Repeat);
-            waveChannel.outputDevice.Init(new AudioFileReader(path));
+            waveChannel.outputDevice.Init(new LoopStream(new AudioFileReader(path)) {
+                EnableLooping = waveChannel.repeat,
+            });
             waveChannel.outputDevice.Volume = volume;
             waveChannel.outputDevice.Play();
         }
@@ -153,6 +156,78 @@ namespace SRCTestForm.Resoruces
             {
                 // ignore
             }
+        }
+    }
+
+    // https://markheath.net/post/looped-playback-in-net-with-naudio
+    /// <summary>
+    /// Stream for looping playback
+    /// </summary>
+    public class LoopStream : WaveStream
+    {
+        WaveStream sourceStream;
+
+        /// <summary>
+        /// Creates a new Loop stream
+        /// </summary>
+        /// <param name="sourceStream">The stream to read from. Note: the Read method of this stream should return 0 when it reaches the end
+        /// or else we will not loop to the start again.</param>
+        public LoopStream(WaveStream sourceStream)
+        {
+            this.sourceStream = sourceStream;
+            this.EnableLooping = true;
+        }
+
+        /// <summary>
+        /// Use this to turn looping on or off
+        /// </summary>
+        public bool EnableLooping { get; set; }
+
+        /// <summary>
+        /// Return source stream's wave format
+        /// </summary>
+        public override WaveFormat WaveFormat
+        {
+            get { return sourceStream.WaveFormat; }
+        }
+
+        /// <summary>
+        /// LoopStream simply returns
+        /// </summary>
+        public override long Length
+        {
+            get { return sourceStream.Length; }
+        }
+
+        /// <summary>
+        /// LoopStream simply passes on positioning to source stream
+        /// </summary>
+        public override long Position
+        {
+            get { return sourceStream.Position; }
+            set { sourceStream.Position = value; }
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            int totalBytesRead = 0;
+
+            while (totalBytesRead < count)
+            {
+                int bytesRead = sourceStream.Read(buffer, offset + totalBytesRead, count - totalBytesRead);
+                if (bytesRead == 0)
+                {
+                    if (sourceStream.Position == 0 || !EnableLooping)
+                    {
+                        // something wrong with the source stream
+                        break;
+                    }
+                    // loop
+                    sourceStream.Position = 0;
+                }
+                totalBytesRead += bytesRead;
+            }
+            return totalBytesRead;
         }
     }
 }
