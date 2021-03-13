0e04a51b2738561242f2432981bebfb8b88efbaa
koudenpa <koudenpa@hotmail.com>
2021-03-14 01:54:45 +0900

    Clear/RestoreEventCmd持ってきた。

---
+++ SRC.Sharp/SRCCore/CmdDatas/Commands/Event/ClearEventCmd.cs
@@ -0,0 +1,57 @@
+﻿using SRCCore.Events;
+
+namespace SRCCore.CmdDatas.Commands
+{
+    public class ClearEventCmd : CmdData
+    {
+        public ClearEventCmd(SRC src, EventDataLine eventData) : base(src, CmdType.ClearEventCmd, eventData)
+        {
+        }
+
+        protected override int ExecInternal()
+        {
+            switch (ArgNum)
+            {
+                case 2:
+                    {
+                        string arglname = GetArgAsString((short)2);
+                        ret = Event_Renamed.FindLabel(ref arglname);
+                        if (ret > 0)
+                        {
+                            Event_Renamed.ClearLabel(ret);
+                        }
+
+                        break;
+                    }
+
+                case 1:
+                    {
+                        if (Event_Renamed.CurrentLabel > 0)
+                        {
+                            Event_Renamed.ClearLabel(Event_Renamed.CurrentLabel);
+                        }
+
+                        break;
+                    }
+
+                default:
+                    {
+                        Event_Renamed.EventErrorMessage = "ClearEventコマンドの引数の数が違います";
+                        ;
+#error Cannot convert ErrorStatementSyntax - see comment for details
+                        /* Cannot convert ErrorStatementSyntax, CONVERSION ERROR: Conversion for ErrorStatement not implemented, please report this issue in 'Error(0)' at character 160477
+
+
+                        Input:
+                                        Error(0)
+
+                         */
+                        break;
+                    }
+            }
+
+            ExecClearEventCmdRet = LineNum + 1;
+            return EventData.ID + 1;
+        }
+    }
+}
---
+++ SRC.Sharp/SRCCore/CmdDatas/Commands/Event/RestoreEventCmd.cs
@@ -0,0 +1,33 @@
+﻿using SRCCore.Events;
+
+namespace SRCCore.CmdDatas.Commands
+{
+    public class RestoreEventCmd : CmdData
+    {
+        public RestoreEventCmd(SRC src, EventDataLine eventData) : base(src, CmdType.RestoreEventCmd, eventData)
+        {
+        }
+
+        protected override int ExecInternal()
+        {
+            if ((int)ArgNum != 2)
+            {
+                Event_Renamed.EventErrorMessage = "RestoreEventコマンドの引数の数が違います";
+                ;
+#error Cannot convert ErrorStatementSyntax - see comment for details
+                /* Cannot convert ErrorStatementSyntax, CONVERSION ERROR: Conversion for ErrorStatement not implemented, please report this issue in 'Error(0)' at character 439974
+
+
+                Input:
+                            Error(0)
+
+                 */
+            }
+
+            string arglname = GetArgAsString(2);
+            Event_Renamed.RestoreLabel(ref arglname);
+
+            return EventData.ID + 1;
+        }
+    }
+}
