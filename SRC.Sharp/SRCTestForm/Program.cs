eec55df1dce15fea51104beaf1b37f687ab9c66e
koudenpa <koudenpa@hotmail.com>
2021-03-15 07:31:56 +0900

    プロジェクト整理。

--- SRC.Sharp/SRCTestForm/Program.cs
+++ SRC.Sharp/SRCTestForm/Program.cs
@@ -1,8 +1,5 @@
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SRCTestForm
