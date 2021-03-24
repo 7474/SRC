5b85491b201ec9cc578f8dd51d581921a53c27d5
koudenpa <koudenpa@hotmail.com>
2021-03-25 00:44:02 +0900

    Unit関数

---
+++ SRC.Sharp/SRCCore/Expressions/Functions/Unit.cs
@@ -0,0 +1,43 @@
+﻿using SRCCore.Lib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SRCCore.Expressions.Functions
+{
+    public class Unit : AFunction
+    {
+        public override ValueType Invoke(SRC SRC, ValueType etype, string[] @params, int pcount, bool[] is_term, out string str_result, out double num_result)
+        {
+            str_result = "";
+            num_result = 0d;
+
+            switch (pcount)
+            {
+                case 1:
+                    var pname = SRC.Expression.GetValueAsString(@params[1], is_term[1]);
+
+                    if (SRC.UList.IsDefined2(pname))
+                    {
+                        str_result = SRC.UList.Item2(pname).Name;
+                    }
+                    else if (SRC.PList.IsDefined(pname))
+                    {
+                        str_result = SRC.PList.Item(pname)?.Unit?.Name ?? "";
+                    }
+
+                    break;
+
+                case 0:
+                    if (SRC.Event.SelectedUnitForEvent != null)
+                    {
+                        str_result = SRC.Event.SelectedUnitForEvent.Name;
+                    }
+
+                    break;
+            }
+
+            return ValueType.StringType;
+        }
+    }
+}
