6d96908d505af0e7089848b86a803c09aba43dfd
koudenpa <koudenpa@hotmail.com>
2021-03-30 03:41:35 +0900

    配列の添え字取る。

---
+++ SRC.Sharp/SRCCore/Extensions/StringExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SRCCore.Extensions
+{
+    public static class StringExtension
+    {
+        private static readonly Regex ArrayIndexRegex = new Regex("^.+?\\[(.+)\\]$");
+        public static string ArrayIndexByName(this string vname)
+        {
+            var m = ArrayIndexRegex.Match(vname);
+            if(m.Success)
+            {
+                return m.Groups[1].Value;
+            }
+            return "";
+        }
+    }
+}
---
+++ SRC.Sharp/SRCCoreTests/Extensions/StringExtensionTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SRCCore.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRCCore.Extensions.Tests
+{
+    [TestClass()]
+    public class StringExtensionTests
+    {
+        [TestMethod()]
+        public void ArrayIndexByNameTest()
+        {
+            Assert.AreEqual("", "hoge".ArrayIndexByName());
+            Assert.AreEqual("1", "hoge[1]".ArrayIndexByName());
+            Assert.AreEqual("abc", "hoge[abc]".ArrayIndexByName());
+            Assert.AreEqual("fuga[abc]", "hoge[fuga[abc]]".ArrayIndexByName());
+        }
+    }
+}
