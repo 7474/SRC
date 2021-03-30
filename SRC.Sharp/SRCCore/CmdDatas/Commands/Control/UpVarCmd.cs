88a9e8b5d23e47787dc3a66b9015787f92118a29
koudenpa <koudenpa@hotmail.com>
2021-03-31 00:26:45 +0900

    UpVarCmd

---
+++ SRC.Sharp/SRCCore/CmdDatas/Commands/Control/UpVarCmd.cs
@@ -0,0 +1,19 @@
+﻿using SRCCore.Events;
+using SRCCore.Exceptions;
+using SRCCore.Lib;
+
+namespace SRCCore.CmdDatas.Commands
+{
+    public class UpVarCmd : CmdData
+    {
+        public UpVarCmd(SRC src, EventDataLine eventData) : base(src, CmdType.UpVarCmd, eventData)
+        {
+        }
+
+        protected override int ExecInternal()
+        {
+            Event.UpVarLevel = Event.UpVarLevel + 1;
+            return EventData.NextID;
+        }
+    }
+}
