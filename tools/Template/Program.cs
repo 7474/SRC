82552a3b5d8c6dec3cf47f829e874c7db9c26388
koudenpa <koudenpa@hotmail.com>
2021-04-30 02:43:08 +0900

    テンプレート

---
+++ tools/Template/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var type = args.First();
+            var template = File.ReadAllText($"{type}Template.cs.txt");
+            foreach (var name in args.Skip(1))
+            {
+                File.WriteAllText(
+                    Path.Combine("tmp", $"{name}{type}.cs"),
+                    template.Replace("{0}", name)
+                    );
+            }
+        }
+    }
+}
