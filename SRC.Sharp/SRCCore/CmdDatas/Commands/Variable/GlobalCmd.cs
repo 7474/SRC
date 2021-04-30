6fd01e55f9f88324aa50bea7b5db4d468c38211a
koudenpa <koudenpa@hotmail.com>
2021-05-01 07:01:06 +0900

    GlobalCmd

--- SRC.Sharp/SRCCore/CmdDatas/Commands/Variable/GlobalCmd.cs
+++ SRC.Sharp/SRCCore/CmdDatas/Commands/Variable/GlobalCmd.cs
@@ -1,10 +1,11 @@
 using SRCCore.Events;
 using SRCCore.Exceptions;
 using SRCCore.Expressions;
+using SRCCore.VB;
 
 namespace SRCCore.CmdDatas.Commands
 {
-    public class GlobalCmd   : CmdData
+    public class GlobalCmd : CmdData
     {
         public GlobalCmd(SRC src, EventDataLine eventData) : base(src, CmdType.GlobalCmd, eventData)
         {
@@ -12,40 +13,25 @@ namespace SRCCore.CmdDatas.Commands
 
         protected override int ExecInternal()
         {
-            //            int ExecGlobalCmdRet = default;
-            //            string vname;
-            //            short i;
-            //            var loopTo = ArgNum;
-            //            for (i = 2; i <= loopTo; i++)
-            //            {
-            //                vname = GetArg(i);
-            //                if (Strings.InStr(vname, "\"") > 0)
-            //                {
-            //                    Event.EventErrorMessage = "変数名「" + vname + "」が不正です";
-            //                    ;
-            //#error Cannot convert ErrorStatementSyntax - see comment for details
-            //                    /* Cannot convert ErrorStatementSyntax, CONVERSION ERROR: Conversion for ErrorStatement not implemented, please report this issue in 'Error(0)' at character 285764
+            for (var i = 2; i <= ArgNum; i++)
+            {
+                var vname = GetArg(i);
+                if (Strings.InStr(vname, "\"") > 0)
+                {
+                    throw new EventErrorException(this, "変数名「" + vname + "」が不正です");
+                }
 
+                if (Strings.Asc(vname) == 36) // $
+                {
+                    vname = Strings.Mid(vname, 2);
+                }
 
-            //                    Input:
-            //                                    Error(0)
+                if (!Expression.IsGlobalVariableDefined(vname))
+                {
+                    Expression.DefineGlobalVariable(vname);
+                }
+            }
 
-            //                     */
-            //                }
-
-            //                if (Strings.Asc(vname) == 36) // $
-            //                {
-            //                    vname = Strings.Mid(vname, 2);
-            //                }
-
-            //                if (!Expression.IsGlobalVariableDefined(vname))
-            //                {
-            //                    Expression.DefineGlobalVariable(vname);
-            //                }
-            //            }
-
-            //            ExecGlobalCmdRet = LineNum + 1;
-            //            return ExecGlobalCmdRet;
             return EventData.NextID;
         }
     }
