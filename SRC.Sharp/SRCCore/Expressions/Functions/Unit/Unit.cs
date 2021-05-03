fd1860f299eb37ab7a84f1d9c1695c77f4f6f52e
koudenpa <koudenpa@hotmail.com>
2021-05-03 20:56:49 +0900

    ユニット情報関数

--- SRC.Sharp/SRCCore/Expressions/Functions/Unit/Unit.cs
+++ SRC.Sharp/SRCCore/Expressions/Functions/Unit/Unit.cs
@@ -419,15 +419,18 @@ namespace SRCCore.Expressions.Functions
         }
     }
 
-
-    public class SpecialPower : AFunction
+    public class SpecialPower : AUnitFunction
     {
-        protected override ValueType InvokeInternal(SRC SRC, ValueType etype, string[] @params, int pcount, bool[] is_term, out string str_result, out double num_result)
+        protected override int OptionArgCount => 1;
+        protected override ValueType InvokeInternal(SRC SRC, Units.Unit unit, ValueType etype, string[] @params, int pcount, bool[] is_term, out string str_result, out double num_result)
         {
             str_result = "";
             num_result = 0d;
 
-            // TODO Impl Specialpower
+            var name = pcount == 1
+                ? SRC.Expression.GetValueAsString(@params[1], is_term[1])
+                : SRC.Expression.GetValueAsString(@params[2], is_term[2]);
+            num_result = unit?.IsSpecialPowerInEffect(name) ?? false ? 1d : 0d;
 
             if (etype == ValueType.StringType)
             {
@@ -441,104 +444,37 @@ namespace SRCCore.Expressions.Functions
         }
     }
 
-
-    public class Mind : AFunction
+    public class Mind : SpecialPower
     {
-        protected override ValueType InvokeInternal(SRC SRC, ValueType etype, string[] @params, int pcount, bool[] is_term, out string str_result, out double num_result)
-        {
-            str_result = "";
-            num_result = 0d;
-
-            // TODO Impl Mind
-
-            if (etype == ValueType.StringType)
-            {
-                str_result = GeneralLib.FormatNum(num_result);
-                return ValueType.StringType;
-            }
-            else
-            {
-                return ValueType.NumericType;
-            }
-        }
     }
 
-
-    public class Status : AFunction
+    public class Status : AUnitFunction
     {
-        protected override ValueType InvokeInternal(SRC SRC, ValueType etype, string[] @params, int pcount, bool[] is_term, out string str_result, out double num_result)
+        protected override ValueType InvokeInternal(SRC SRC, Units.Unit unit, ValueType etype, string[] @params, int pcount, bool[] is_term, out string str_result, out double num_result)
         {
-            str_result = "";
+            str_result = unit?.Status ?? "";
             num_result = 0d;
-
-            // TODO Impl Status
-
-            if (etype == ValueType.StringType)
-            {
-                str_result = GeneralLib.FormatNum(num_result);
-                return ValueType.StringType;
-            }
-            else
-            {
-                return ValueType.NumericType;
-            }
-        }
-    }
-
-
-    public class Unit : AFunction
-    {
-        protected override ValueType InvokeInternal(SRC SRC, ValueType etype, string[] @params, int pcount, bool[] is_term, out string str_result, out double num_result)
-        {
-            str_result = "";
-            num_result = 0d;
-
-            switch (pcount)
-            {
-                case 1:
-                    var pname = SRC.Expression.GetValueAsString(@params[1], is_term[1]);
-
-                    if (SRC.UList.IsDefined2(pname))
-                    {
-                        str_result = SRC.UList.Item2(pname).Name;
-                    }
-                    else if (SRC.PList.IsDefined(pname))
-                    {
-                        str_result = SRC.PList.Item(pname)?.Unit?.Name ?? "";
-                    }
-
-                    break;
-
-                case 0:
-                    if (SRC.Event.SelectedUnitForEvent != null)
-                    {
-                        str_result = SRC.Event.SelectedUnitForEvent.Name;
-                    }
-
-                    break;
-            }
-
             return ValueType.StringType;
         }
     }
-    public class UnitID : AFunction
+
+    public class Unit : AUnitFunction
     {
-        protected override ValueType InvokeInternal(SRC SRC, ValueType etype, string[] @params, int pcount, bool[] is_term, out string str_result, out double num_result)
+        protected override ValueType InvokeInternal(SRC SRC, Units.Unit unit, ValueType etype, string[] @params, int pcount, bool[] is_term, out string str_result, out double num_result)
         {
-            str_result = "";
+            str_result = unit?.Name ?? "";
             num_result = 0d;
+            return ValueType.StringType;
+        }
+    }
 
-            // TODO Impl Unitid
-
-            if (etype == ValueType.StringType)
-            {
-                str_result = GeneralLib.FormatNum(num_result);
-                return ValueType.StringType;
-            }
-            else
-            {
-                return ValueType.NumericType;
-            }
+    public class UnitID : AUnitFunction
+    {
+        protected override ValueType InvokeInternal(SRC SRC, Units.Unit unit, ValueType etype, string[] @params, int pcount, bool[] is_term, out string str_result, out double num_result)
+        {
+            str_result = unit?.ID ?? "";
+            num_result = 0d;
+            return ValueType.StringType;
         }
     }
 }
