22e55a31c6e3192023d2f0edaf82b73d321b1ef0
koudenpa <koudenpa@hotmail.com>
2021-05-03 16:51:27 +0900

    ToHex失敗

---
+++ SRC.Sharp/SRCCore/Extensions/ColorExtension.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace SRCCore.Extensions
+{
+    public static class ColorExtension
+    {
+        public static string ToHexString(this Color color)
+        {
+            return ColorTranslator.ToHtml(color);
+        }
+    }
+}
---
+++ SRC.Sharp/SRCCoreTests/Extensions/ColorExtensionTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SRCCore.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRCCore.Extensions.Tests
+{
+    [TestClass()]
+    public class ColorExtensionTests
+    {
+        [TestMethod()]
+        public void ToHexStringTest()
+        {
+            Assert.AreEqual("#ff0000", Color.Red.ToHexString());
+        }
+    }
+}
