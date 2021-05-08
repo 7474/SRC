4bfdeeeacff3b33b01ad9c21396a4710c4fcc599
koudenpa <koudenpa@hotmail.com>
2021-05-09 02:29:13 +0900

    Fix Ask終了

--- SRC.Sharp/SRCCore/Events/Event.data.cs
+++ SRC.Sharp/SRCCore/Events/Event.data.cs
@@ -682,11 +682,10 @@ namespace SRCCore.Events
                                 "拡大",
                                 "連続表示",
                                 "キャンセル可",
-                                "終了",
                             };
-                            if (command.ArgNum < 3
-                                || command.GetArgs().ToList()
+                            if ((command.ArgNum < 3 || command.GetArgs().ToList()
                                     .FindIndex(x => opts.Contains(x.strArg)) < 3)
+                                && !command.GetArgs().Select(x => x.strArg).Contains("終了"))
                             {
                                 cmdStack.Push(CmdType.AskCmd);
                                 cmdPosStack.Push(command.EventData.ID);
