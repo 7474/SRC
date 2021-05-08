3088d6f69e7636a782c3618023e919313110ab89
koudenpa <koudenpa@hotmail.com>
2021-05-08 19:27:08 +0900

    Fix 余白量

--- SRC.Sharp/SRCSharpForm/Extensions/GraphicsExtension.cs
+++ SRC.Sharp/SRCSharpForm/Extensions/GraphicsExtension.cs
@@ -4,13 +4,17 @@ namespace SRCSharpForm.Extensions
 {
     public static class GraphicsExtension
     {
+        private static Font RefFont = SystemFonts.DefaultFont;
         public static SizeF MeasureStringWithoutRightMargin(this Graphics g, string str, Font font)
         {
             var size = g.MeasureString(str, font);
             if (str.Length == str.TrimEnd().Length)
             {
                 // 終端が空白文字列ではない場合はMeasureが付与する余白をカットする
-                size.Width -= font.Size / 2f;
+                // https://github.com/dotnet/runtime/blob/33033b22eccf50550451387ac8927ad1b5f17768/src/libraries/System.Drawing.Common/src/System/Drawing/Graphics.cs#L1565
+                // 最終的には GdipMeasureString に移譲されていて余白の付与量が具体的にどうなのかサッとは分からなかった。
+                // 雰囲気デフォルトっぽい文字サイズの 0.5em 位な感じはするのでそのくらいにしておく。
+                size.Width -= RefFont.Size / 2f;
             }
 
             return size;
