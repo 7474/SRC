58da02878fa785e07d40475ff02f3552de28ed23
koudenpa <koudenpa@hotmail.com>
2021-05-12 03:02:15 +0900

    Init cypress

---
+++ SRC.Sharp/SRCTestBlazor/E2E/cypress/integration/index.js
@@ -0,0 +1,14 @@
+﻿
+describe('Init Blazor Application', function () {
+    it('Blazorアプリケーションとしてのレンダリングが行われる', function () {
+        cy.visit('https://7474.github.io/SRC/');
+        cy.get('input[type="file"]').should('exist');
+    });
+});
+
+describe('Load Data', function () {
+    it('適当なデータが読み込まれる', function () {
+        // TODO
+        cy.visit('https://7474.github.io/SRC/');
+    });
+});
