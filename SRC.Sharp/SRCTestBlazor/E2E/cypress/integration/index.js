c1e47ccb05f4d5ba5d5de55d249f51bbeb2db6ce
koudenpa <koudenpa@hotmail.com>
2021-05-12 03:11:07 +0900

    データロードのケース

--- SRC.Sharp/SRCTestBlazor/E2E/cypress/integration/index.js
+++ SRC.Sharp/SRCTestBlazor/E2E/cypress/integration/index.js
@@ -1,14 +1,19 @@
 ﻿
+const ROOT_URL = 'https://7474.github.io/SRC/';
+
 describe('Init Blazor Application', function () {
     it('Blazorアプリケーションとしてのレンダリングが行われる', function () {
-        cy.visit('https://7474.github.io/SRC/');
+        cy.visit(ROOT_URL);
         cy.get('input[type="file"]').should('exist');
     });
 });
 
 describe('Load Data', function () {
     it('適当なデータが読み込まれる', function () {
-        // TODO
-        cy.visit('https://7474.github.io/SRC/');
+        cy.visit(ROOT_URL);
+        cy.get('.navbar-burger ').click();
+        cy.contains('Titles').click();
+        cy.contains('∀ガンダム').click();
+        cy.contains('∀ガンダム(前期)').should('exist');
     });
 });
