148374bb03a2969560c2b94ae655d18b3886b116
koudenpa <koudenpa@hotmail.com>
2021-05-17 04:09:43 +0900

    cypress からのアクセス時には乱数を固定化してみる

--- SRC.Sharp/SRCTestBlazor/Program.cs
+++ SRC.Sharp/SRCTestBlazor/Program.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.JSInterop;
 using SRCTestBlazor.Models;
 using System;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -13,18 +15,31 @@ namespace SRCTestBlazor
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+            // https://github.com/dotnet/aspnetcore/issues/24461#issuecomment-667068936
+            var js = (IJSInProcessRuntime)builder.Services.BuildServiceProvider().GetRequiredService<IJSRuntime>();
+            var startupParams = js.Invoke<string[]>("getStartupParams");
+
             builder.Services
                 .AddScoped(sp => new HttpClient
                 {
                     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
                 })
                 .AddSingleton<SrcDataContainer>()
-                .AddSingleton<Random>();
+                .AddSingleton<Random>(sp =>
+                {
+                    var random = new Random();
+                    return isE2ETest(startupParams) ? new Random(0) : new Random();
+                });
 
             builder.RootComponents.Add<App>("#app");
 
             var host = builder.Build();
             await host.RunAsync();
+        }
+
+        private static bool isE2ETest(string[] startupParams)
+        {
+            return startupParams?.Any(arg => arg == "--cypress") ?? false;
         }
     }
 }
