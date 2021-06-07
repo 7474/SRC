6a4b655d3623b5634eaeb4237e7405395e3dc20d
koudenpa <koudenpa@hotmail.com>
2021-06-08 00:03:29 +0900

    ContinueCmd

--- SRC.Sharp/SRCCore/CmdDatas/Commands/Stage/ContinueCmd.cs
+++ SRC.Sharp/SRCCore/CmdDatas/Commands/Stage/ContinueCmd.cs
@@ -1,5 +1,11 @@
 using SRCCore.Events;
 using SRCCore.Exceptions;
+using SRCCore.Extensions;
+using SRCCore.Lib;
+using SRCCore.Pilots;
+using SRCCore.Units;
+using SRCCore.VB;
+using System.Linq;
 
 namespace SRCCore.CmdDatas.Commands
 {
@@ -30,138 +36,121 @@ namespace SRCCore.CmdDatas.Commands
 
             SRC.GUIStatus.ClearUnitStatus();
 
-            // TODO Impl 経験し取得などの後処理
-            //// 追加経験値を得るパイロットや破壊されたユニットがいなければ処理をスキップ
-            //n = 0;
-            //foreach (Unit currentU in SRC.UList.Items)
-            //{
-            //    u = currentU;
-            //    if (u.Party0 == "味方")
-            //    {
-            //        if (u.Status == "出撃" | u.Status == "格納" | u.Status == "破壊")
-            //        {
-            //            n = 1;
-            //            break;
-            //        }
-            //    }
-            //}
+            // 追加経験値を得るパイロットや破壊されたユニットがいなければ処理をスキップ
+            var n = 0;
+            foreach (Unit u in SRC.UList.Items)
+            {
+                if (u.Party0 == "味方")
+                {
+                    if (u.Status == "出撃" || u.Status == "格納" || u.Status == "破壊")
+                    {
+                        n = 1;
+                        break;
+                    }
+                }
+            }
 
-            //if (n == 0)
-            //{
-            //    SRC.Turn = 0;
-            //}
+            if (n == 0)
+            {
+                SRC.Turn = 0;
+            }
 
-            //// 追加経験値を収得
-            //if (SRC.Turn > 0 & !Expression.IsOptionDefined("追加経験値無効"))
-            //{
-            //    GUI.OpenMessageForm(u1: null, u2: null);
-            //    n = 0;
-            //    msg = "";
-            //    foreach (Pilot p in SRC.PList)
-            //    {
-            //        if (p.Party != "味方")
-            //        {
-            //            goto NextPilot;
-            //        }
+            // 追加経験値を収得
+            if (SRC.Turn > 0 && !Expression.IsOptionDefined("追加経験値無効"))
+            {
+                GUI.OpenMessageForm(u1: null, u2: null);
+                n = 0;
+                var msg = "";
+                foreach (Pilot p in SRC.PList.Items)
+                {
+                    if (p.Party != "味方")
+                    {
+                        goto NextPilot;
+                    }
 
-            //        if (p.MaxSP == 0)
-            //        {
-            //            goto NextPilot;
-            //        }
+                    if (p.MaxSP == 0)
+                    {
+                        goto NextPilot;
+                    }
 
-            //        if (p.Unit is null)
-            //        {
-            //            goto NextPilot;
-            //        }
+                    if (p.Unit is null)
+                    {
+                        goto NextPilot;
+                    }
 
-            //        if (p.Unit.Status != "出撃" & p.Unit.Status != "格納")
-            //        {
-            //            goto NextPilot;
-            //        }
+                    if (p.Unit.Status != "出撃" && p.Unit.Status != "格納")
+                    {
+                        goto NextPilot;
+                    }
 
-            //        plevel = p.Level;
-            //        p.Exp = p.Exp + 2 * p.SP;
+                    var plevel = p.Level;
+                    p.Exp = p.Exp + 2 * p.SP;
 
-            //        // 追加パイロットや暴走時パイロットに関する処理
-            //        if (p.Unit.CountPilot() > 0 & !p.IsSupport(p.Unit))
-            //        {
-            //            // 追加パイロットがメインパイロットの場合
-            //            if (ReferenceEquals(p, p.Unit.Pilot(1)) & !ReferenceEquals(p, p.Unit.MainPilot()) & p.Unit.MainPilot().MaxSP > 0)
-            //            {
-            //                goto NextPilot;
-            //            }
+                    // 追加パイロットや暴走時パイロットに関する処理
+                    if (p.Unit.CountPilot() > 0 && !p.IsSupport(p.Unit))
+                    {
+                        // 追加パイロットがメインパイロットの場合
+                        if (ReferenceEquals(p, p.Unit.Pilots.First()) && !ReferenceEquals(p, p.Unit.MainPilot()) && p.Unit.MainPilot().MaxSP > 0)
+                        {
+                            goto NextPilot;
+                        }
 
-            //            // 追加パイロットがメインパイロットではなくなった場合
-            //            if (!ReferenceEquals(p, p.Unit.MainPilot()))
-            //            {
-            //                // 自分がユニットのパイロット一覧に含まれているか判定
-            //                var loopTo = p.Unit.CountPilot();
-            //                for (i = 1; i <= loopTo; i++)
-            //                {
-            //                    Pilot localPilot() { object argIndex1 = i; var ret = p.Unit.Pilot(argIndex1); return ret; }
+                        // 追加パイロットがメインパイロットではなくなった場合
+                        if (!ReferenceEquals(p, p.Unit.MainPilot()))
+                        {
+                            // 自分がユニットのパイロット一覧に含まれているか判定
+                            if (!p.Unit.Pilots.Contains(p))
+                            {
+                                goto NextPilot;
+                            }
+                        }
+                    }
 
-            //                    if (ReferenceEquals(p, localPilot()))
-            //                    {
-            //                        break;
-            //                    }
-            //                }
+                    if (plevel == p.Level)
+                    {
+                        msg = msg + ";" + p.get_Nickname(false) + " 経験値 +" + SrcFormatter.Format(2 * p.SP);
+                    }
+                    else
+                    {
+                        msg = msg + ";" + p.get_Nickname(false) + " 経験値 +" + SrcFormatter.Format(2 * p.SP) + " レベルアップ！（Lv" + SrcFormatter.Format(p.Level) + "）";
+                    }
 
-            //                if (i > p.Unit.CountPilot())
-            //                {
-            //                    goto NextPilot;
-            //                }
-            //            }
-            //        }
+                    n = (n + 1);
+                    if (n == 4)
+                    {
+                        GUI.DisplayMessage("システム", Strings.Mid(msg, 2));
+                        msg = "";
+                        n = 0;
+                    }
 
-            //        if (plevel == p.Level)
-            //        {
-            //            msg = msg + ";" + p.get_Nickname(false) + " 経験値 +" + SrcFormatter.Format(2 * p.SP);
-            //        }
-            //        else
-            //        {
-            //            msg = msg + ";" + p.get_Nickname(false) + " 経験値 +" + SrcFormatter.Format(2 * p.SP) + " レベルアップ！（Lv" + SrcFormatter.Format(p.Level) + "）";
-            //        }
+                NextPilot:
+                    ;
+                }
 
-            //        n = (n + 1);
-            //        if (n == 4)
-            //        {
-            //            GUI.DisplayMessage("システム", Strings.Mid(msg, 2));
-            //            msg = "";
-            //            n = 0;
-            //        }
+                if (n > 0)
+                {
+                    GUI.DisplayMessage("システム", Strings.Mid(msg, 2));
+                }
 
-            //    NextPilot:
-            //        ;
-            //    }
-
-            //    if (n > 0)
-            //    {
-            //        GUI.DisplayMessage("システム", Strings.Mid(msg, 2));
-            //    }
-
-            //    GUI.CloseMessageForm();
-            //}
+                GUI.CloseMessageForm();
+            }
 
             GUI.MainFormHide();
 
             // エピローグイベントを実行
             if (Event.IsEventDefined("エピローグ"))
             {
-                // TODO Impl エピローグイベントを実行
-                //// ハイパーモードや変身、能力コピーを解除
-                //foreach (Unit currentU1 in SRC.UList)
-                //{
-                //    u = currentU1;
-                //    if (u.Status != "他形態" & u.Status != "旧主形態" & u.Status != "旧形態")
-                //    {
-                //        if (u.IsFeatureAvailable("ノーマルモード"))
-                //        {
-                //            string localLIndex() { object argIndex1 = "ノーマルモード"; string arglist = u.FeatureData(argIndex1); var ret = GeneralLib.LIndex(arglist, 1); return ret; }
-
-                //            u.Transform(localLIndex());
-                //        }
-                //    }
-                //}
+                // ハイパーモードや変身、能力コピーを解除
+                foreach (Unit u in SRC.UList.Items.CloneList())
+                {
+                    if (u.Status != "他形態" && u.Status != "旧主形態" && u.Status != "旧形態")
+                    {
+                        if (u.IsFeatureAvailable("ノーマルモード"))
+                        {
+                            u.Transform(GeneralLib.LIndex(u.FeatureData("ノーマルモード"), 1));
+                        }
+                    }
+                }
 
                 if (Event.IsEventDefined("エピローグ", true))
                 {
