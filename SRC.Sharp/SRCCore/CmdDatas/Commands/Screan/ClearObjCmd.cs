bd86ccf42c94b845fd561852adb5e8e34361de2f
koudenpa <koudenpa@hotmail.com>
2021-06-10 22:44:50 +0900

    ClearObjCmd

--- SRC.Sharp/SRCCore/CmdDatas/Commands/Screan/ClearObjCmd.cs
+++ SRC.Sharp/SRCCore/CmdDatas/Commands/Screan/ClearObjCmd.cs
@@ -1,4 +1,6 @@
 using SRCCore.Events;
+using SRCCore.Exceptions;
+using SRCCore.Extensions;
 using System;
 
 namespace SRCCore.CmdDatas.Commands
@@ -11,7 +13,8 @@ namespace SRCCore.CmdDatas.Commands
 
         protected override int ExecInternal()
         {
-            n = ArgNum;
+            var without_refresh = false;
+            var n = ArgNum;
             if (n > 1)
             {
                 if (GetArgAsString(n) == "非同期")
@@ -25,85 +28,44 @@ namespace SRCCore.CmdDatas.Commands
             {
                 case 2:
                     {
-                        oname = GetArgAsString(2);
-                        var loopTo = Information.UBound(Event.HotPointList);
-                        for (i = 1; i <= loopTo; i++)
-                        {
-                            if ((Event.HotPointList[i].Name ?? "") == (oname ?? ""))
-                            {
-                                break;
-                            }
-                        }
-
-                        if (i <= Information.UBound(Event.HotPointList))
-                        {
-                            {
-                                var withBlock = Event.HotPointList[i];
-                                if (My.MyProject.Forms.frmToolTip.Visible && (Event.SelectedAlternative ?? "") == (withBlock.Name ?? ""))
-                                {
-                                    // ツールチップを消す
-                                    My.MyProject.Forms.frmToolTip.Hide();
-                                    // マウスカーソルを元に戻す
-                                    GUI.MainForm.picMain(0).MousePointer = 0;
-                                }
-                            }
-
-                            var loopTo1 = (Information.UBound(Event.HotPointList) - 1);
-                            for (j = i; j <= loopTo1; j++)
-                                // UPGRADE_WARNING: オブジェクト HotPointList(j) の既定プロパティを解決できませんでした。 詳細については、'ms-help://MS.VSCC.v90/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"' をクリックしてください。
-                                Event.HotPointList[j] = Event.HotPointList[j + 1];
-                            Array.Resize(Event.HotPointList, Information.UBound(Event.HotPointList));
-                        }
-
+                        var oname = GetArgAsString(2);
+                        Event.HotPointList.RemoveItem(x => x.Name == oname);
                         break;
                     }
 
                 case 1:
                     {
-                        Event.HotPointList = new Event.HotPoint[1];
+                        Event.HotPointList.Clear();
                         break;
                     }
 
                 default:
-                    {
-                        Event.EventErrorMessage = "ClearObjコマンドの引数の数が違います";
-                        ;
-#error Cannot convert ErrorStatementSyntax - see comment for details
-                        /* Cannot convert ErrorStatementSyntax, CONVERSION ERROR: Conversion for ErrorStatement not implemented, please report this issue in 'Error(0)' at character 183065
-
-
-                        Input:
-                                        Error(0)
-
-                         */
-                        break;
-                    }
+                    throw new EventErrorException(this, "ClearObjコマンドの引数の数が違います");
             }
 
-            ExecClearObjCmdRet = LineNum + 1;
+            // TODO Update hotpoint
+            //// まだマウスカーソルがホットポイント上にあるか？
+            //var loopTo2 = Information.UBound(Event.HotPointList);
+            //for (i = 1; i <= loopTo2; i++)
+            //{
+            //    {
+            //        var withBlock1 = Event.HotPointList[i];
+            //        if (withBlock1.Left <= GUI.MouseX && GUI.MouseX < withBlock1.Left + withBlock1.width && withBlock1.Top <= GUI.MouseY && GUI.MouseY < withBlock1.Top + withBlock1.Height)
+            //        {
+            //            return ExecClearObjCmdRet;
+            //        }
+            //    }
+            //}
 
-            // まだマウスカーソルがホットポイント上にあるか？
-            var loopTo2 = Information.UBound(Event.HotPointList);
-            for (i = 1; i <= loopTo2; i++)
-            {
-                {
-                    var withBlock1 = Event.HotPointList[i];
-                    if (withBlock1.Left <= GUI.MouseX && GUI.MouseX < withBlock1.Left + withBlock1.width && withBlock1.Top <= GUI.MouseY && GUI.MouseY < withBlock1.Top + withBlock1.Height)
-                    {
-                        return ExecClearObjCmdRet;
-                    }
-                }
-            }
+            //// ツールチップを消す
+            //My.MyProject.Forms.frmToolTip.Hide();
+            //if (!without_refresh)
+            //{
+            //    GUI.MainForm.picMain(0).Refresh();
+            //}
 
-            // ツールチップを消す
-            My.MyProject.Forms.frmToolTip.Hide();
-            if (!without_refresh)
-            {
-                GUI.MainForm.picMain(0).Refresh();
-            }
-
-            // マウスカーソルを元に戻す
-            GUI.MainForm.picMain(0).MousePointer = 0;
+            //// マウスカーソルを元に戻す
+            //GUI.MainForm.picMain(0).MousePointer = 0;
             return EventData.NextID;
         }
     }
