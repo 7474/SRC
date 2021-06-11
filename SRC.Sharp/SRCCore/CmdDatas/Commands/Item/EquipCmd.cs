7b8c094812215263ae8979f16db805c1bec33096
koudenpa <koudenpa@hotmail.com>
2021-06-12 04:32:27 +0900

    EquipCmd

--- SRC.Sharp/SRCCore/CmdDatas/Commands/Item/EquipCmd.cs
+++ SRC.Sharp/SRCCore/CmdDatas/Commands/Item/EquipCmd.cs
@@ -1,5 +1,8 @@
 using SRCCore.Events;
-using System;
+using SRCCore.Exceptions;
+using SRCCore.Items;
+using SRCCore.Units;
+using System.Linq;
 
 namespace SRCCore.CmdDatas.Commands
 {
@@ -11,6 +14,8 @@ namespace SRCCore.CmdDatas.Commands
 
         protected override int ExecInternal()
         {
+            Unit u;
+            string iname;
             switch (ArgNum)
             {
                 case 3:
@@ -28,71 +33,44 @@ namespace SRCCore.CmdDatas.Commands
                     }
 
                 default:
-                    {
-                        Event.EventErrorMessage = "Equipコマンドの引数の数が違います";
-                        ;
-#error Cannot convert ErrorStatementSyntax - see comment for details
-                        /* Cannot convert ErrorStatementSyntax, CONVERSION ERROR: Conversion for ErrorStatement not implemented, please report this issue in 'Error(0)' at character 223923
-
-
-                        Input:
-                                        Error(0)
-
-                         */
-                        break;
-                    }
+                    throw new EventErrorException(this, "Equipコマンドの引数の数が違います");
             }
 
             // 大文字・小文字、ひらがな・かたかなの違いを正しく判定できるように、
             // 名前をデータのそれとあわせる
             if (SRC.IDList.IsDefined(iname))
             {
-                ItemData localItem() { object argIndex1 = iname; var ret = SRC.IDList.Item(argIndex1); return ret; }
-
-                iname = localItem().Name;
+                iname = SRC.IDList.Item(iname).Name;
             }
 
             // 装備するアイテムを検索 or 作成
-            bool localIsDefined() { object argIndex1 = iname; var ret = SRC.IDList.IsDefined(argIndex1); return ret; }
-
+            Item itm;
             if (SRC.IList.IsDefined(iname))
             {
-                Item localItem1() { object argIndex1 = (object)iname; var ret = SRC.IList.Item(argIndex1); return ret; }
-
-                if ((iname ?? "") == (localItem1().Name ?? ""))
+                if (iname == SRC.IList.Item(iname).Name)
                 {
                     // アイテム名で指定した場合
                     if (u.Party0 == "味方")
                     {
                         // まずは装備されてないものを探す
-                        foreach (Item currentItm in SRC.IList)
+                        itm = SRC.IList.List
+                           .Where(x => x.Exist)
+                           .Where(x => x.Name == iname)
+                           .FirstOrDefault(x => x.Unit == null);
+                        if (itm == null)
                         {
-                            itm = currentItm;
-                            {
-                                var withBlock = itm;
-                                if ((withBlock.Name ?? "") == (iname ?? "") && withBlock.Unit is null && withBlock.Exist)
-                                {
-                                    goto EquipItem;
-                                }
-                            }
+                            // なかったら装備されているものを…
+                            itm = SRC.IList.List
+                               .Where(x => x.Exist)
+                               .Where(x => x.Name == iname)
+                               .FirstOrDefault(x => x.Unit != null && x.Unit.Party0 == "味方");
+
                         }
-                        // なかったら装備されているものを…
-                        foreach (Item currentItm1 in SRC.IList)
+                        if (itm == null)
                         {
-                            itm = currentItm1;
-                            {
-                                var withBlock1 = itm;
-                                if ((withBlock1.Name ?? "") == (iname ?? "") && withBlock1.Unit is object && withBlock1.Exist)
-                                {
-                                    if (withBlock1.Unit.Party0 == "味方")
-                                    {
-                                        goto EquipItem;
-                                    }
-                                }
-                            }
+                            // それでもなければ新たに作成
+                            itm = SRC.IList.Add(iname);
                         }
-                        // それでもなければ新たに作成
-                        itm = SRC.IList.Add(iname);
                     }
                     else
                     {
@@ -102,101 +80,49 @@ namespace SRCCore.CmdDatas.Commands
                 else
                 {
                     // アイテムＩＤで指定した場合
-                    itm = SRC.IList.Item((object)iname);
+                    itm = SRC.IList.Item(iname);
                 }
             }
-            else if (localIsDefined())
+            else if (SRC.IDList.IsDefined(iname))
             {
                 itm = SRC.IList.Add(iname);
             }
             else
             {
-                Event.EventErrorMessage = "「" + iname + "」というアイテムは存在しません";
-                ;
-#error Cannot convert ErrorStatementSyntax - see comment for details
-                /* Cannot convert ErrorStatementSyntax, CONVERSION ERROR: Conversion for ErrorStatement not implemented, please report this issue in 'Error(0)' at character 225275
-
-
-                Input:
-                            Error(0)
-
-                 */
+                throw new EventErrorException(this, "「" + iname + "」というアイテムは存在しません");
             }
 
-        EquipItem:
-            ;
-
             // アイテムを装備
-            string ubitmap;
-            short rank_lv = default, cmd_lv = default, support_lv = default;
-            if (itm is object)
+            if (itm != null)
             {
+                if (itm.Exist)
                 {
-                    var withBlock2 = itm;
-                    if (withBlock2.Exist)
+                    if (itm.Unit != null)
                     {
-                        if (withBlock2.Unit is object)
+                        itm.Unit.DeleteItem(itm);
+                    }
+
+                    u.AddItem(itm);
+
+                    // ユニット画像が変化した？
+                    if (u.Status == "出撃")
+                    {
+                        if (!GUI.IsPictureVisible && Map.IsStatusView)
                         {
-                            withBlock2.Unit.DeleteItem(withBlock2.ID);
+                            GUI.PaintUnitBitmap(u);
                         }
+                    }
 
-                        {
-                            var withBlock3 = u;
-                            ubitmap = withBlock3.get_Bitmap(false);
-                            if (withBlock3.CountPilot() > 0)
-                            {
-                                {
-                                    var withBlock4 = withBlock3.MainPilot();
-                                    cmd_lv = withBlock4.SkillLevel("指揮", ref_mode: "");
-                                    rank_lv = withBlock4.SkillLevel("階級", ref_mode: "");
-                                    support_lv = withBlock4.SkillLevel("広域サポート", ref_mode: "");
-                                }
-                            }
+                    // 支援効果が変化した？
+                    if (u.Status == "出撃")
+                    {
+                        SRC.PList.UpdateSupportMod(u);
+                    }
 
-                            withBlock3.AddItem(itm);
-
-                            // ユニット画像が変化した？
-                            if ((ubitmap ?? "") != (withBlock3.get_Bitmap(false) ?? ""))
-                            {
-                                withBlock3.BitmapID = GUI.MakeUnitBitmap(u);
-                                var loopTo = withBlock3.CountOtherForm();
-                                for (i = 1; i <= loopTo; i++)
-                                {
-                                    Unit localOtherForm() { object argIndex1 = i; var ret = withBlock3.OtherForm(argIndex1); return ret; }
-
-                                    localOtherForm().BitmapID = 0;
-                                }
-
-                                if (withBlock3.Status == "出撃")
-                                {
-                                    if (!GUI.IsPictureVisible && !string.IsNullOrEmpty(Map.MapFileName))
-                                    {
-                                        GUI.PaintUnitBitmap(u);
-                                    }
-                                }
-                            }
-
-                            // 支援効果が変化した？
-                            if (withBlock3.CountPilot() > 0)
-                            {
-                                {
-                                    var withBlock5 = withBlock3.MainPilot();
-                                    if (cmd_lv != withBlock5.SkillLevel("指揮", ref_mode: "") || rank_lv != withBlock5.SkillLevel("階級", ref_mode: "") || support_lv != withBlock5.SkillLevel("広域サポート", ref_mode: ""))
-                                    {
-                                        if (u.Status == "出撃")
-                                        {
-                                            SRC.PList.UpdateSupportMod(u);
-                                        }
-                                    }
-                                }
-                            }
-
-                            // 最大弾数が変化した？
-                            if (itm.IsFeatureAvailable("最大弾数増加"))
-                            {
-                                withBlock3.FullSupply();
-                            }
-                        }
+                    // 最大弾数が変化した？
+                    if (itm.IsFeatureAvailable("最大弾数増加"))
+                    {
+                        u.FullSupply();
                     }
                 }
             }
