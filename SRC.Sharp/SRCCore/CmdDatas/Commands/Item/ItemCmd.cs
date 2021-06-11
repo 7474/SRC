7f3504e26364d64c0c052ff37d7297bc66f2d741
koudenpa <koudenpa@hotmail.com>
2021-06-12 00:29:37 +0900

    ItemCmd

--- SRC.Sharp/SRCCore/CmdDatas/Commands/Item/ItemCmd.cs
+++ SRC.Sharp/SRCCore/CmdDatas/Commands/Item/ItemCmd.cs
@@ -1,4 +1,5 @@
 using SRCCore.Events;
+using SRCCore.Exceptions;
 using System;
 
 namespace SRCCore.CmdDatas.Commands
@@ -11,8 +12,24 @@ namespace SRCCore.CmdDatas.Commands
 
         protected override int ExecInternal()
         {
-            throw new NotImplementedException();
-            //return EventData.NextID;
+            string iname;
+            switch (ArgNum)
+            {
+                case 2:
+                    iname = GetArgAsString(2);
+                    break;
+
+                default:
+                    throw new EventErrorException(this, "Itemコマンドの引数の数が違います");
+            }
+
+            if (!SRC.IDList.IsDefined(iname))
+            {
+                throw new EventErrorException(this, "「" + iname + "」というアイテムは存在しません");
+            }
+
+            SRC.IList.Add(iname);
+            return EventData.NextID;
         }
     }
 }
