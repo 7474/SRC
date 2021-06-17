2c6d7ff694fac1133fa3dcf6d04c5be03975669a
koudenpa <koudenpa@hotmail.com>
2021-06-18 01:33:41 +0900

    TransformCmd

--- SRC.Sharp/SRCCore/CmdDatas/CmdParser.cs
+++ SRC.Sharp/SRCCore/CmdDatas/CmdParser.cs
@@ -581,7 +581,7 @@ namespace SRCCore.CmdDatas
                         return new TelopCmd(src, data);
 
                     case "transform":
-                        return new NotImplementedCmd(src, data);
+                        return new TransformCmd(src, data);
 
                     case "unit":
                         return new UnitCmd(src, data);
--- SRC.Sharp/SRCCore/CmdDatas/Commands/Unit/TransformCmd.cs
+++ SRC.Sharp/SRCCore/CmdDatas/Commands/Unit/TransformCmd.cs
@@ -11,8 +11,72 @@ namespace SRCCore.CmdDatas.Commands
 
         protected override int ExecInternal()
         {
-            throw new NotImplementedException();
-            //return EventData.NextID;
+            Unit u;
+            string tname;
+            switch (ArgNum)
+            {
+                case 3:
+                    {
+                        u = GetArgAsUnit(2);
+                        tname = GetArgAsString(3);
+                        break;
+                    }
+
+                case 2:
+                    {
+                        u = Event.SelectedUnitForEvent;
+                        tname = GetArgAsString(2);
+                        break;
+                    }
+
+                default:
+                    {
+                        Event.EventErrorMessage = "Transformコマンドの引数の数が違います";
+                        ;
+#error Cannot convert ErrorStatementSyntax - see comment for details
+                        /* Cannot convert ErrorStatementSyntax, CONVERSION ERROR: Conversion for ErrorStatement not implemented, please report this issue in 'Error(0)' at character 513842
+
+
+                        Input:
+                                        Error(0)
+
+                         */
+                        break;
+                    }
+            }
+
+            if ((u.Name ?? "") == (tname ?? ""))
+            {
+                // 元々指定された形態になっていたので変形の必要なし
+                ExecTransformCmdRet = LineNum + 1;
+                return ExecTransformCmdRet;
+            }
+
+            // 変形
+            u.Transform(tname);
+
+            // グローバル変数の更新
+            if (ReferenceEquals(u, Commands.SelectedUnit))
+            {
+                Commands.SelectedUnit = u.CurrentForm();
+            }
+
+            if (ReferenceEquals(u, Event.SelectedUnitForEvent))
+            {
+                Event.SelectedUnitForEvent = u.CurrentForm();
+            }
+
+            if (ReferenceEquals(u, Commands.SelectedTarget))
+            {
+                Commands.SelectedTarget = u.CurrentForm();
+            }
+
+            if (ReferenceEquals(u, Event.SelectedTargetForEvent))
+            {
+                Event.SelectedTargetForEvent = u.CurrentForm();
+            }
+
+            return EventData.NextID;
         }
     }
 }
