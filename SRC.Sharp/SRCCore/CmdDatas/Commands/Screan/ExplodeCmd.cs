5772c995558124b8d0bce6970b07cb05ac16d3eb
koudenpa <koudenpa@hotmail.com>
2021-06-20 06:58:52 +0900

    ExplodeCmd

--- SRC.Sharp/SRCCore/CmdDatas/Commands/Screan/ExplodeCmd.cs
+++ SRC.Sharp/SRCCore/CmdDatas/Commands/Screan/ExplodeCmd.cs
@@ -1,5 +1,5 @@
 using SRCCore.Events;
-using System;
+using SRCCore.Exceptions;
 
 namespace SRCCore.CmdDatas.Commands
 {
@@ -11,46 +11,33 @@ namespace SRCCore.CmdDatas.Commands
 
         protected override int ExecInternal()
         {
-            throw new NotImplementedException();
-            //            string esize;
-            //            short tx, ty;
-            //            switch (ArgNum)
-            //            {
-            //                case 2:
-            //                    {
-            //                        esize = GetArgAsString(2);
-            //                        tx = GUI.MapX;
-            //                        ty = GUI.MapY;
-            //                        break;
-            //                    }
+            string esize;
+            int tx, ty;
+            switch (ArgNum)
+            {
+                case 2:
+                    {
+                        esize = GetArgAsString(2);
+                        tx = GUI.MapX;
+                        ty = GUI.MapY;
+                        break;
+                    }
 
-            //                case 4:
-            //                    {
-            //                        esize = GetArgAsString(2);
-            //                        tx = GetArgAsLong(3);
-            //                        ty = GetArgAsLong(4);
-            //                        break;
-            //                    }
+                case 4:
+                    {
+                        esize = GetArgAsString(2);
+                        tx = GetArgAsLong(3);
+                        ty = GetArgAsLong(4);
+                        break;
+                    }
 
-            //                default:
-            //                    {
-            //                        Event.EventErrorMessage = "Explodeコマンドの引数の数が違います";
-            //                        ;
-            //#error Cannot convert ErrorStatementSyntax - see comment for details
-            //                        /* Cannot convert ErrorStatementSyntax, CONVERSION ERROR: Conversion for ErrorStatement not implemented, please report this issue in 'Error(0)' at character 235613
+                default:
+                    throw new EventErrorException(this, "Explodeコマンドの引数の数が違います");
+            }
 
-
-            //                        Input:
-            //                                        Error(0)
-
-            //                         */
-            //                        break;
-            //                    }
-            //            }
-
-            //            // 爆発の表示
-            //            Effect.ExplodeAnimation(esize, tx, ty);
-            //return EventData.NextID;
+            // 爆発の表示
+            SRC.Effect.ExplodeAnimation(esize, tx, ty);
+            return EventData.NextID;
         }
     }
 }
