b3874d50c46c08eb768683ff6c4b0ebe8916f78a
koudenpa <koudenpa@hotmail.com>
2021-06-20 04:01:01 +0900

    ForgetCmd

--- SRC.Sharp/SRCCore/CmdDatas/Commands/Other/ForgetCmd.cs
+++ SRC.Sharp/SRCCore/CmdDatas/Commands/Other/ForgetCmd.cs
@@ -1,5 +1,5 @@
 using SRCCore.Events;
-using System;
+using SRCCore.Exceptions;
 
 namespace SRCCore.CmdDatas.Commands
 {
@@ -11,42 +11,15 @@ namespace SRCCore.CmdDatas.Commands
 
         protected override int ExecInternal()
         {
-            throw new NotImplementedException();
-            //            string tname;
-            //            short i, j;
-            //            if (ArgNum != 2)
-            //            {
-            //                Event.EventErrorMessage = "Forgetコマンドの引数の数が違います";
-            //                ;
-            //#error Cannot convert ErrorStatementSyntax - see comment for details
-            //                /* Cannot convert ErrorStatementSyntax, CONVERSION ERROR: Conversion for ErrorStatement not implemented, please report this issue in 'Error(0)' at character 282918
+            if (ArgNum != 2)
+            {
+                throw new EventErrorException(this, "Forgetコマンドの引数の数が違います");
+            }
 
+            var tname = GetArgAsString(2);
+            SRC.Titles.Remove(tname);
 
-            //                Input:
-            //                            Error(0)
-
-            //                 */
-            //            }
-
-            //            tname = GetArgAsString(2);
-            //            var loopTo = Information.UBound(SRC.Titles);
-            //            for (i = 1; i <= loopTo; i++)
-            //            {
-            //                if ((tname ?? "") == (SRC.Titles[i] ?? ""))
-            //                {
-            //                    break;
-            //                }
-            //            }
-
-            //            if (i <= Information.UBound(SRC.Titles))
-            //            {
-            //                var loopTo1 = Information.UBound(SRC.Titles);
-            //                for (j = (i + 1); j <= loopTo1; j++)
-            //                    SRC.Titles[j - 1] = SRC.Titles[j];
-            //                Array.Resize(SRC.Titles, Information.UBound(SRC.Titles));
-            //            }
-
-            //return EventData.NextID;
+            return EventData.NextID;
         }
     }
 }
