be12f392acfae72bd0aca4e916d6bc07b86825a5
koudenpa <koudenpa@hotmail.com>
2021-06-25 02:42:08 +0900

    RecoverENCmd

--- SRC.Sharp/SRCCore/CmdDatas/Commands/Unit/RecoverENCmd.cs
+++ SRC.Sharp/SRCCore/CmdDatas/Commands/Unit/RecoverENCmd.cs
@@ -1,5 +1,6 @@
 using SRCCore.Events;
-using System;
+using SRCCore.Exceptions;
+using SRCCore.Units;
 
 namespace SRCCore.CmdDatas.Commands
 {
@@ -11,57 +12,44 @@ namespace SRCCore.CmdDatas.Commands
 
         protected override int ExecInternal()
         {
-            throw new NotImplementedException();
-            //            Unit u;
-            //            double per;
-            //            switch (ArgNum)
-            //            {
-            //                case 3:
-            //                    {
-            //                        u = GetArgAsUnit(2, true);
-            //                        per = GetArgAsDouble(3);
-            //                        break;
-            //                    }
+            Unit u;
+            double per;
+            switch (ArgNum)
+            {
+                case 3:
+                    {
+                        u = GetArgAsUnit(2, true);
+                        per = GetArgAsDouble(3);
+                        break;
+                    }
 
-            //                case 2:
-            //                    {
-            //                        u = Event.SelectedUnitForEvent;
-            //                        per = GetArgAsDouble(2);
-            //                        break;
-            //                    }
+                case 2:
+                    {
+                        u = Event.SelectedUnitForEvent;
+                        per = GetArgAsDouble(2);
+                        break;
+                    }
 
-            //                default:
-            //                    {
-            //                        Event.EventErrorMessage = "RecoverENコマンドの引数の数が違います";
-            //                        ;
-            //#error Cannot convert ErrorStatementSyntax - see comment for details
-            //                        /* Cannot convert ErrorStatementSyntax, CONVERSION ERROR: Conversion for ErrorStatement not implemented, please report this issue in 'Error(0)' at character 405679
+                default:
+                    throw new EventErrorException(this, "RecoverENコマンドの引数の数が違います");
+            }
 
+            if (u is object)
+            {
+                {
+                    var withBlock = u;
+                    withBlock.RecoverEN(per);
+                    withBlock.Update();
+                    if (withBlock.EN == 0 && withBlock.Status == "出撃")
+                    {
+                        GUI.PaintUnitBitmap(u);
+                    }
 
-            //                        Input:
-            //                                        Error(0)
-
-            //                         */
-            //                        break;
-            //                    }
-            //            }
-
-            //            if (u is object)
-            //            {
-            //                {
-            //                    var withBlock = u;
-            //                    withBlock.RecoverEN(per);
-            //                    withBlock.Update();
-            //                    if (withBlock.EN == 0 && withBlock.Status == "出撃")
-            //                    {
-            //                        GUI.PaintUnitBitmap(u);
-            //                    }
-
-            //                    withBlock.CheckAutoHyperMode();
-            //                    withBlock.CheckAutoNormalMode();
-            //                }
-            //            }
-            //return EventData.NextID;
+                    withBlock.CheckAutoHyperMode();
+                    withBlock.CheckAutoNormalMode();
+                }
+            }
+            return EventData.NextID;
         }
     }
 }
