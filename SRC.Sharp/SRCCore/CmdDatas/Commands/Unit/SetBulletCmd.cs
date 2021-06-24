93d2a5b513270ef97b635a1dcc94c3c8a7ac54cb
koudenpa <koudenpa@hotmail.com>
2021-06-25 00:41:03 +0900

    SetBulletCmd

--- SRC.Sharp/SRCCore/CmdDatas/Commands/Unit/SetBulletCmd.cs
+++ SRC.Sharp/SRCCore/CmdDatas/Commands/Unit/SetBulletCmd.cs
@@ -1,5 +1,8 @@
 using SRCCore.Events;
-using System;
+using SRCCore.Exceptions;
+using SRCCore.Lib;
+using SRCCore.Units;
+using SRCCore.VB;
 
 namespace SRCCore.CmdDatas.Commands
 {
@@ -11,89 +14,59 @@ namespace SRCCore.CmdDatas.Commands
 
         protected override int ExecInternal()
         {
-            throw new NotImplementedException();
-            //            string wname;
-            //            short wid, num;
-            //            Unit u;
-            //            switch (ArgNum)
-            //            {
-            //                case 4:
-            //                    {
-            //                        u = GetArgAsUnit(2);
-            //                        wname = GetArgAsString(3);
-            //                        num = GetArgAsLong(4);
-            //                        break;
-            //                    }
+            string wname;
+            int wid, num;
+            Unit u;
+            switch (ArgNum)
+            {
+                case 4:
+                    {
+                        u = GetArgAsUnit(2);
+                        wname = GetArgAsString(3);
+                        num = GetArgAsLong(4);
+                        break;
+                    }
 
-            //                case 3:
-            //                    {
-            //                        u = Event.SelectedUnitForEvent;
-            //                        wname = GetArgAsString(2);
-            //                        num = GetArgAsLong(3);
-            //                        break;
-            //                    }
+                case 3:
+                    {
+                        u = Event.SelectedUnitForEvent;
+                        wname = GetArgAsString(2);
+                        num = GetArgAsLong(3);
+                        break;
+                    }
 
-            //                default:
-            //                    {
-            //                        Event.EventErrorMessage = "SetBulletコマンドの引数の数が違います";
-            //                        ;
-            //#error Cannot convert ErrorStatementSyntax - see comment for details
-            //                        /* Cannot convert ErrorStatementSyntax, CONVERSION ERROR: Conversion for ErrorStatement not implemented, please report this issue in 'Error(0)' at character 451174
+                default:
+                    throw new EventErrorException(this, "SetBulletコマンドの引数の数が違います");
+            }
 
+            if (Information.IsNumeric(wname))
+            {
+                wid = GeneralLib.StrToLng(wname);
+                if (wid < 1 || u.CountWeapon() < wid)
+                {
+                    throw new EventErrorException(this, "武器の番号「" + wname + "」が間違っています");
+                }
+            }
+            else
+            {
+                var loopTo = u.CountWeapon();
+                for (wid = 1; wid <= loopTo; wid++)
+                {
+                    if ((u.Weapon(wid).Name ?? "") == (wname ?? ""))
+                    {
+                        break;
+                    }
+                }
 
-            //                        Input:
-            //                                        Error(0)
+                if (wid < 1 || u.CountWeapon() < wid)
+                {
+                    throw new EventErrorException(this, u.Name + "は武器「" + wname + "」を持っていません");
+                }
+            }
 
-            //                         */
-            //                        break;
-            //                    }
-            //            }
-
-            //            if (Information.IsNumeric(wname))
-            //            {
-            //                wid = GeneralLib.StrToLng(wname);
-            //                if (wid < 1 || u.CountWeapon() < wid)
-            //                {
-            //                    Event.EventErrorMessage = "武器の番号「" + wname + "」が間違っています";
-            //                    ;
-            //#error Cannot convert ErrorStatementSyntax - see comment for details
-            //                    /* Cannot convert ErrorStatementSyntax, CONVERSION ERROR: Conversion for ErrorStatement not implemented, please report this issue in 'Error(0)' at character 451471
-
-
-            //                    Input:
-            //                                        Error(0)
-
-            //                     */
-            //                }
-            //            }
-            //            else
-            //            {
-            //                var loopTo = u.CountWeapon();
-            //                for (wid = 1; wid <= loopTo; wid++)
-            //                {
-            //                    if ((u.Weapon(wid).Name ?? "") == (wname ?? ""))
-            //                    {
-            //                        break;
-            //                    }
-            //                }
-
-            //                if (wid < 1 || u.CountWeapon() < wid)
-            //                {
-            //                    Event.EventErrorMessage = u.Name + "は武器「" + wname + "」を持っていません";
-            //                    ;
-            //#error Cannot convert ErrorStatementSyntax - see comment for details
-            //                    /* Cannot convert ErrorStatementSyntax, CONVERSION ERROR: Conversion for ErrorStatement not implemented, please report this issue in 'Error(0)' at character 451756
-
-
-            //                    Input:
-            //                                        Error(0)
-
-            //                     */
-            //                }
-            //            }
-
-            //            u.SetBullet(wid, GeneralLib.MinLng(num, u.MaxBullet(wid)));
-            //return EventData.NextID;
+            var uw = u.Weapon(wid);
+            uw.SetBullet(GeneralLib.MinLng(num, uw.MaxBullet()));
+            return EventData.NextID;
         }
     }
 }
