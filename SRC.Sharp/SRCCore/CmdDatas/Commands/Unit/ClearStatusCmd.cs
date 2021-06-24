af1730b730d6bd9fdf00fd17b46d9e31f671f82e
koudenpa <koudenpa@hotmail.com>
2021-06-25 02:04:32 +0900

    ClearStatusCmd

--- SRC.Sharp/SRCCore/CmdDatas/Commands/Unit/ClearStatusCmd.cs
+++ SRC.Sharp/SRCCore/CmdDatas/Commands/Unit/ClearStatusCmd.cs
@@ -1,5 +1,6 @@
 using SRCCore.Events;
-using System;
+using SRCCore.Exceptions;
+using SRCCore.Units;
 
 namespace SRCCore.CmdDatas.Commands
 {
@@ -11,54 +12,41 @@ namespace SRCCore.CmdDatas.Commands
 
         protected override int ExecInternal()
         {
-            throw new NotImplementedException();
-            //            string sname;
-            //            Unit u;
-            //            switch (ArgNum)
-            //            {
-            //                case 3:
-            //                    {
-            //                        u = GetArgAsUnit(2);
-            //                        sname = GetArgAsString(3);
-            //                        break;
-            //                    }
+            string sname;
+            Unit u;
+            switch (ArgNum)
+            {
+                case 3:
+                    {
+                        u = GetArgAsUnit(2);
+                        sname = GetArgAsString(3);
+                        break;
+                    }
 
-            //                case 2:
-            //                    {
-            //                        u = Event.SelectedUnitForEvent;
-            //                        sname = GetArgAsString(2);
-            //                        break;
-            //                    }
+                case 2:
+                    {
+                        u = Event.SelectedUnitForEvent;
+                        sname = GetArgAsString(2);
+                        break;
+                    }
 
-            //                default:
-            //                    {
-            //                        Event.EventErrorMessage = "ClearStatusコマンドの引数の数が違います";
-            //                        ;
-            //#error Cannot convert ErrorStatementSyntax - see comment for details
-            //                        /* Cannot convert ErrorStatementSyntax, CONVERSION ERROR: Conversion for ErrorStatement not implemented, please report this issue in 'Error(0)' at character 188579
+                default:
+                    throw new EventErrorException(this, "ClearStatusコマンドの引数の数が違います");
+            }
 
-
-            //                        Input:
-            //                                        Error(0)
-
-            //                         */
-            //                        break;
-            //                    }
-            //            }
-
-            //            {
-            //                var withBlock = u;
-            //                if (withBlock.IsConditionSatisfied(sname))
-            //                {
-            //                    withBlock.DeleteCondition(sname);
-            //                    withBlock.Update();
-            //                    if (withBlock.Status == "出撃")
-            //                    {
-            //                        GUI.PaintUnitBitmap(u);
-            //                    }
-            //                }
-            //            }
-            //return EventData.NextID;
+            {
+                var withBlock = u;
+                if (withBlock.IsConditionSatisfied(sname))
+                {
+                    withBlock.DeleteCondition(sname);
+                    withBlock.Update();
+                    if (withBlock.Status == "出撃")
+                    {
+                        GUI.PaintUnitBitmap(u);
+                    }
+                }
+            }
+            return EventData.NextID;
         }
     }
 }
