d0ce29f69cf5e2c6b9007f0625ccd4de2094599e
koudenpa <koudenpa@hotmail.com>
2021-06-25 02:23:21 +0900

    JoinCmd

--- SRC.Sharp/SRCCore/CmdDatas/Commands/Unit/JoinCmd.cs
+++ SRC.Sharp/SRCCore/CmdDatas/Commands/Unit/JoinCmd.cs
@@ -1,5 +1,7 @@
 using SRCCore.Events;
-using System;
+using SRCCore.Exceptions;
+using SRCCore.Units;
+using System.Linq;
 
 namespace SRCCore.CmdDatas.Commands
 {
@@ -12,138 +14,75 @@ namespace SRCCore.CmdDatas.Commands
 
         protected override int ExecInternal()
         {
-            throw new NotImplementedException();
-            //            var pname = default(string);
-            //            var u = default(Unit);
-            //            short i;
-            //            switch (ArgNum)
-            //            {
-            //                case 2:
-            //                    {
-            //                        pname = GetArgAsString(2);
-            //                        bool localIsDefined() { object argIndex1 = (object)pname; var ret = SRC.NPDList.IsDefined(argIndex1); return ret; }
+            string pname = "";
+            Unit u;
+            switch (ArgNum)
+            {
+                case 2:
+                    {
+                        pname = GetArgAsString(2);
+                        if (SRC.PList.IsDefined(pname))
+                        {
+                            u = SRC.PList.Item(pname).Unit;
+                        }
+                        else if (SRC.NPDList.IsDefined(pname))
+                        {
+                            pname = "IsAway(" + SRC.NPDList.Item(pname).Name + ")";
+                            if (Expression.IsGlobalVariableDefined(pname))
+                            {
+                                Expression.UndefineVariable(pname);
+                            }
 
-            //                        bool localIsDefined1() { object argIndex1 = (object)pname; var ret = SRC.UList.IsDefined(argIndex1); return ret; }
+                            return EventData.NextID;
+                        }
+                        else if (SRC.UList.IsDefined(pname))
+                        {
+                            if ((pname ?? "") == (SRC.UList.Item(pname).ID ?? ""))
+                            {
+                                u = SRC.UList.Item(pname);
+                            }
+                            else
+                            {
+                                u = SRC.UList.Items
+                                    .FirstOrDefault(x => x.Name == pname && x.Party0 == "味方" && x.CurrentForm().Status == "離脱")
+                                    ?.CurrentForm();
+                            }
+                        }
+                        else
+                        {
+                            throw new EventErrorException(this, "「" + pname + "」というパイロットまたはユニットが見つかりません");
+                        }
 
-            //                        if (SRC.PList.IsDefined((object)pname))
-            //                        {
-            //                            Pilot localItem() { object argIndex1 = (object)pname; var ret = SRC.PList.Item(argIndex1); return ret; }
+                        break;
+                    }
 
-            //                            u = localItem().Unit;
-            //                        }
-            //                        else if (localIsDefined())
-            //                        {
-            //                            NonPilotData localItem1() { object argIndex1 = (object)pname; var ret = SRC.NPDList.Item(argIndex1); return ret; }
+                case 1:
+                    {
+                        u = Event.SelectedUnitForEvent;
+                        break;
+                    }
 
-            //                            pname = "IsAway(" + localItem1().Name + ")";
-            //                            if (Expression.IsGlobalVariableDefined(pname))
-            //                            {
-            //                                Expression.UndefineVariable(pname);
-            //                            }
+                default:
+                    throw new EventErrorException(this, "Joinコマンドの引数の数が違います");
+            }
 
-            //                            ExecJoinCmdRet = LineNum + 1;
-            //                            return ExecJoinCmdRet;
-            //                        }
-            //                        else if (localIsDefined1())
-            //                        {
-            //                            Unit localItem2() { object argIndex1 = (object)pname; var ret = SRC.UList.Item(argIndex1); return ret; }
+            if (u is null)
+            {
+                if (SRC.PList.IsDefined(pname))
+                {
+                    SRC.PList.Item(pname).Away = false;
+                }
+            }
+            else
+            {
+                u.Status = "待機";
+                foreach (var p in u.AllRawPilots)
+                {
+                    p.Away = false;
+                }
+            }
 
-            //                            if ((pname ?? "") == (localItem2().ID ?? ""))
-            //                            {
-            //                                // UPGRADE_WARNING: オブジェクト u の既定プロパティを解決できませんでした。 詳細については、'ms-help://MS.VSCC.v90/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"' をクリックしてください。
-            //                                u = SRC.UList.Item((object)pname);
-            //                            }
-            //                            else
-            //                            {
-            //                                foreach (Unit currentU in SRC.UList)
-            //                                {
-            //                                    u = currentU;
-            //                                    {
-            //                                        var withBlock = u;
-            //                                        if ((withBlock.Name ?? "") == (pname ?? "") && withBlock.Party0 == "味方" && withBlock.CurrentForm().Status == "離脱")
-            //                                        {
-            //                                            u = withBlock.CurrentForm();
-            //                                            break;
-            //                                        }
-            //                                    }
-            //                                }
-
-            //                                if ((u.Name ?? "") != (pname ?? ""))
-            //                                {
-            //                                    // UPGRADE_NOTE: オブジェクト u をガベージ コレクトするまでこのオブジェクトを破棄することはできません。 詳細については、'ms-help://MS.VSCC.v90/dv_commoner/local/redirect.htm?keyword="6E35BFF6-CD74-4B09-9689-3E1A43DF8969"' をクリックしてください。
-            //                                    u = null;
-            //                                }
-            //                            }
-            //                        }
-            //                        else
-            //                        {
-            //                            Event.EventErrorMessage = "「" + pname + "」というパイロットまたはユニットが見つかりません";
-            //                            ;
-            //#error Cannot convert ErrorStatementSyntax - see comment for details
-            //                            /* Cannot convert ErrorStatementSyntax, CONVERSION ERROR: Conversion for ErrorStatement not implemented, please report this issue in 'Error(0)' at character 299677
-
-
-            //                            Input:
-            //                                                Error(0)
-
-            //                             */
-            //                        }
-
-            //                        break;
-            //                    }
-
-            //                case 1:
-            //                    {
-            //                        u = Event.SelectedUnitForEvent;
-            //                        break;
-            //                    }
-
-            //                default:
-            //                    {
-            //                        Event.EventErrorMessage = "Joinコマンドの引数の数が違います";
-            //                        ;
-            //#error Cannot convert ErrorStatementSyntax - see comment for details
-            //                        /* Cannot convert ErrorStatementSyntax, CONVERSION ERROR: Conversion for ErrorStatement not implemented, please report this issue in 'Error(0)' at character 299864
-
-
-            //                        Input:
-            //                                        Error(0)
-
-            //                         */
-            //                        break;
-            //                    }
-            //            }
-
-            //            if (u is null)
-            //            {
-            //                if (SRC.PList.IsDefined(pname))
-            //                {
-            //                    Pilot localItem3() { object argIndex1 = pname; var ret = SRC.PList.Item(argIndex1); return ret; }
-
-            //                    localItem3().Away = false;
-            //                }
-            //            }
-            //            else
-            //            {
-            //                u.Status = "待機";
-            //                var loopTo = u.CountPilot();
-            //                for (i = 1; i <= loopTo; i++)
-            //                {
-            //                    Pilot localPilot() { object argIndex1 = i; var ret = u.Pilot(argIndex1); return ret; }
-
-            //                    localPilot().Away = false;
-            //                }
-
-            //                var loopTo1 = u.CountSupport();
-            //                for (i = 1; i <= loopTo1; i++)
-            //                {
-            //                    Pilot localSupport() { object argIndex1 = i; var ret = u.Support(argIndex1); return ret; }
-
-            //                    localSupport().Away = false;
-            //                }
-            //            }
-
-            //return EventData.NextID;
+            return EventData.NextID;
         }
     }
 }
