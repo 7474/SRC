597debe33b251e70dd65080e65c73fb7545e1bc2
koudenpa <koudenpa@hotmail.com>
2021-06-25 00:36:30 +0900

    SetStockCmd

--- SRC.Sharp/SRCCore/CmdDatas/Commands/Unit/SetStockCmd.cs
+++ SRC.Sharp/SRCCore/CmdDatas/Commands/Unit/SetStockCmd.cs
@@ -1,5 +1,8 @@
 using SRCCore.Events;
-using System;
+using SRCCore.Exceptions;
+using SRCCore.Lib;
+using SRCCore.Units;
+using SRCCore.VB;
 
 namespace SRCCore.CmdDatas.Commands
 {
@@ -11,89 +14,59 @@ namespace SRCCore.CmdDatas.Commands
 
         protected override int ExecInternal()
         {
-            throw new NotImplementedException();
-            //            string aname;
-            //            short aid, num;
-            //            Unit u;
-            //            switch (ArgNum)
-            //            {
-            //                case 4:
-            //                    {
-            //                        u = GetArgAsUnit(2);
-            //                        aname = GetArgAsString(3);
-            //                        num = GetArgAsLong(4);
-            //                        break;
-            //                    }
+            string aname;
+            int aid, num;
+            Unit u;
+            switch (ArgNum)
+            {
+                case 4:
+                    {
+                        u = GetArgAsUnit(2);
+                        aname = GetArgAsString(3);
+                        num = GetArgAsLong(4);
+                        break;
+                    }
 
-            //                case 3:
-            //                    {
-            //                        u = Event.SelectedUnitForEvent;
-            //                        aname = GetArgAsString(2);
-            //                        num = GetArgAsLong(3);
-            //                        break;
-            //                    }
+                case 3:
+                    {
+                        u = Event.SelectedUnitForEvent;
+                        aname = GetArgAsString(2);
+                        num = GetArgAsLong(3);
+                        break;
+                    }
 
-            //                default:
-            //                    {
-            //                        Event.EventErrorMessage = "SetStockコマンドの引数の数が違います";
-            //                        ;
-            //#error Cannot convert ErrorStatementSyntax - see comment for details
-            //                        /* Cannot convert ErrorStatementSyntax, CONVERSION ERROR: Conversion for ErrorStatement not implemented, please report this issue in 'Error(0)' at character 466229
+                default:
+                    throw new EventErrorException(this, "SetStockコマンドの引数の数が違います");
+            }
 
+            if (Information.IsNumeric(aname))
+            {
+                aid = GeneralLib.StrToLng(aname);
+                if (aid < 1 || u.CountAbility() < aid)
+                {
+                    throw new EventErrorException(this, "アビリティの番号「" + aname + "」が間違っています");
+                }
+            }
+            else
+            {
+                var loopTo = u.CountAbility();
+                for (aid = 1; aid <= loopTo; aid++)
+                {
+                    if ((u.Ability(aid).Data.Name ?? "") == (aname ?? ""))
+                    {
+                        break;
+                    }
+                }
 
-            //                        Input:
-            //                                        Error(0)
+                if (aid < 1 || u.CountAbility() < aid)
+                {
+                    throw new EventErrorException(this, u.Name + "はアビリティ「" + aname + "」を持っていません");
+                }
+            }
 
-            //                         */
-            //                        break;
-            //                    }
-            //            }
-
-            //            if (Information.IsNumeric(aname))
-            //            {
-            //                aid = GeneralLib.StrToLng(aname);
-            //                if (aid < 1 || u.CountAbility() < aid)
-            //                {
-            //                    Event.EventErrorMessage = "アビリティの番号「" + aname + "」が間違っています";
-            //                    ;
-            //#error Cannot convert ErrorStatementSyntax - see comment for details
-            //                    /* Cannot convert ErrorStatementSyntax, CONVERSION ERROR: Conversion for ErrorStatement not implemented, please report this issue in 'Error(0)' at character 466530
-
-
-            //                    Input:
-            //                                        Error(0)
-
-            //                     */
-            //                }
-            //            }
-            //            else
-            //            {
-            //                var loopTo = u.CountAbility();
-            //                for (aid = 1; aid <= loopTo; aid++)
-            //                {
-            //                    if ((u.Ability(aid).Name ?? "") == (aname ?? ""))
-            //                    {
-            //                        break;
-            //                    }
-            //                }
-
-            //                if (aid < 1 || u.CountAbility() < aid)
-            //                {
-            //                    Event.EventErrorMessage = u.Name + "はアビリティ「" + aname + "」を持っていません";
-            //                    ;
-            //#error Cannot convert ErrorStatementSyntax - see comment for details
-            //                    /* Cannot convert ErrorStatementSyntax, CONVERSION ERROR: Conversion for ErrorStatement not implemented, please report this issue in 'Error(0)' at character 466821
-
-
-            //                    Input:
-            //                                        Error(0)
-
-            //                     */
-            //                }
-            //            }
-
-            //            u.SetStock(aid, GeneralLib.MinLng(num, u.MaxStock(aid)));
-            //return EventData.NextID;
+            var ua = u.Ability(aid);
+            ua.SetStock(GeneralLib.MinLng(num, ua.MaxStock()));
+            return EventData.NextID;
         }
     }
 }
