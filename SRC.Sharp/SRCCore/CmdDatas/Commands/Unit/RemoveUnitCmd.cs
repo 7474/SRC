c8a40c4e4302cc439a5f3cc15287dfa3f5698918
koudenpa <koudenpa@hotmail.com>
2021-06-27 17:10:30 +0900

    RemoveUnitCmd

--- SRC.Sharp/SRCCore/CmdDatas/Commands/Unit/RemoveUnitCmd.cs
+++ SRC.Sharp/SRCCore/CmdDatas/Commands/Unit/RemoveUnitCmd.cs
@@ -1,5 +1,6 @@
 using SRCCore.Events;
-using System;
+using SRCCore.Exceptions;
+using System.Linq;
 
 namespace SRCCore.CmdDatas.Commands
 {
@@ -11,181 +12,116 @@ namespace SRCCore.CmdDatas.Commands
 
         protected override int ExecInternal()
         {
-            throw new NotImplementedException();
-            //            string uname;
-            //            Unit u;
-            //            short i, num;
-            //            var opt = default(string);
-            //            num = ArgNum;
-            //            if (num > 1)
-            //            {
-            //                if (GetArgAsString(num) == "非同期")
-            //                {
-            //                    opt = "非同期";
-            //                    num = (num - 1);
-            //                }
-            //            }
+            var num = ArgNum;
+            var opt = "";
+            if (num > 1)
+            {
+                if (GetArgAsString(num) == "非同期")
+                {
+                    opt = "非同期";
+                    num = (num - 1);
+                }
+            }
 
-            //            switch (num)
-            //            {
-            //                case 1:
-            //                    {
-            //                        {
-            //                            var withBlock = Event.SelectedUnitForEvent.CurrentForm();
-            //                            withBlock.Escape(opt);
-            //                            if (withBlock.CountPilot() > 0)
-            //                            {
-            //                                withBlock.Pilot((object)1).GetOff();
-            //                            }
+            switch (num)
+            {
+                case 1:
+                    {
+                        {
+                            var u = Event.SelectedUnitForEvent.CurrentForm();
+                            u.Escape(opt);
+                            if (u.CountPilot() > 0)
+                            {
+                                u.Pilots.First().GetOff();
+                            }
 
-            //                            withBlock.Status = "破棄";
-            //                            var loopTo = withBlock.CountOtherForm();
-            //                            for (i = 1; i <= loopTo; i++)
-            //                            {
-            //                                Unit localOtherForm1() { object argIndex1 = (object)i; var ret = withBlock.OtherForm(argIndex1); return ret; }
+                            u.Status = "破棄";
+                            foreach (var cf in u.OtherForms)
+                            {
+                                if (cf.Status == "他形態")
+                                {
+                                    cf.Status = "破棄";
+                                }
+                            }
+                        }
 
-            //                                if (localOtherForm1().Status == "他形態")
-            //                                {
-            //                                    Unit localOtherForm() { object argIndex1 = (object)i; var ret = withBlock.OtherForm(argIndex1); return ret; }
+                        break;
+                    }
 
-            //                                    localOtherForm().Status = "破棄";
-            //                                }
-            //                            }
-            //                        }
+                case 2:
+                    {
+                        var uname = GetArgAsString(2);
+                        var u = SRC.UList.Item(uname);
 
-            //                        break;
-            //                    }
+                        // ユニットが存在しなければそのまま終了
+                        if (u is null)
+                        {
+                            return EventData.NextID;
+                        }
 
-            //                case 2:
-            //                    {
-            //                        uname = GetArgAsString(2);
-            //                        u = SRC.UList.Item((object)uname);
+                        // ユニットＩＤで指定された場合
+                        if ((u.ID ?? "") == (uname ?? ""))
+                        {
+                            u.Escape(opt);
+                            if (u.CountPilot() > 0)
+                            {
+                                u.Pilots.First().GetOff();
+                            }
 
-            //                        // ユニットが存在しなければそのまま終了
-            //                        if (u is null)
-            //                        {
-            //                            ExecRemoveUnitCmdRet = LineNum + 1;
-            //                            return ExecRemoveUnitCmdRet;
-            //                        }
+                            u.Status = "破棄";
+                            foreach (var cf in u.OtherForms)
+                            {
+                                if (cf.Status == "他形態")
+                                {
+                                    cf.Status = "破棄";
+                                }
+                            }
 
-            //                        // ユニットＩＤで指定された場合
-            //                        if ((u.ID ?? "") == (uname ?? ""))
-            //                        {
-            //                            u.Escape(opt);
-            //                            if (u.CountPilot() > 0)
-            //                            {
-            //                                u.Pilot((object)1).GetOff();
-            //                            }
+                            return EventData.NextID;
+                        }
 
-            //                            u.Status = "破棄";
-            //                            var loopTo1 = u.CountOtherForm();
-            //                            for (i = 1; i <= loopTo1; i++)
-            //                            {
-            //                                Unit localOtherForm3() { object argIndex1 = (object)i; var ret = u.OtherForm(argIndex1); return ret; }
+                        // 大文字・小文字、ひらがな・かたかなの違いを正しく判定できるように、
+                        // 名前をデータのそれとあわせる
+                        if (SRC.UDList.IsDefined(uname))
+                        {
+                            uname = SRC.UDList.Item(uname).Name;
+                        }
 
-            //                                if (localOtherForm3().Status == "他形態")
-            //                                {
-            //                                    Unit localOtherForm2() { object argIndex1 = (object)i; var ret = u.OtherForm(argIndex1); return ret; }
+                        // パイロットが乗ってないユニットを優先
+                        u = SRC.UList.Items.Select(u => u.CurrentForm())
+                            .Where(u => u.Status == "破棄")
+                            .FirstOrDefault(u => u.CountPilot() == 0);
+                        // 見つからなければパイロットが乗っているユニットを削除
+                        if (u == null)
+                        {
+                            u = SRC.UList.Items.Select(u => u.CurrentForm())
+                                .FirstOrDefault(u => u.Status == "破棄");
+                        }
+                        if (u != null)
+                        {
+                            u.Escape(opt);
+                            if (u.CountPilot() > 0)
+                            {
+                                u.Pilots.First().GetOff();
+                            }
 
-            //                                    localOtherForm2().Status = "破棄";
-            //                                }
-            //                            }
+                            u.Status = "破棄";
+                            foreach (var cf in u.OtherForms)
+                            {
+                                if (cf.Status == "他形態")
+                                {
+                                    cf.Status = "破棄";
+                                }
+                            }
+                        }
+                        break;
+                    }
 
-            //                            ExecRemoveUnitCmdRet = LineNum + 1;
-            //                            return ExecRemoveUnitCmdRet;
-            //                        }
+                default:
+                    throw new EventErrorException(this, "RemoveUnitの引数の数が違います");
+            }
 
-            //                        // 大文字・小文字、ひらがな・かたかなの違いを正しく判定できるように、
-            //                        // 名前をデータのそれとあわせる
-            //                        if (SRC.UDList.IsDefined((object)uname))
-            //                        {
-            //                            UnitData localItem() { object argIndex1 = (object)uname; var ret = SRC.UDList.Item(argIndex1); return ret; }
-
-            //                            uname = localItem().Name;
-            //                        }
-
-            //                        // パイロットが乗ってないユニットを優先
-            //                        foreach (Unit currentU in SRC.UList)
-            //                        {
-            //                            u = currentU;
-            //                            {
-            //                                var withBlock1 = u.CurrentForm();
-            //                                if ((withBlock1.Name ?? "") == (uname ?? "") && withBlock1.Status != "破棄")
-            //                                {
-            //                                    if (withBlock1.CountPilot() == 0)
-            //                                    {
-            //                                        withBlock1.Escape(opt);
-            //                                        withBlock1.Status = "破棄";
-            //                                        var loopTo2 = withBlock1.CountOtherForm();
-            //                                        for (i = 1; i <= loopTo2; i++)
-            //                                        {
-            //                                            Unit localOtherForm5() { object argIndex1 = (object)i; var ret = withBlock1.OtherForm(argIndex1); return ret; }
-
-            //                                            if (localOtherForm5().Status == "他形態")
-            //                                            {
-            //                                                Unit localOtherForm4() { object argIndex1 = (object)i; var ret = withBlock1.OtherForm(argIndex1); return ret; }
-
-            //                                                localOtherForm4().Status = "破棄";
-            //                                            }
-            //                                        }
-
-            //                                        ExecRemoveUnitCmdRet = LineNum + 1;
-            //                                        return ExecRemoveUnitCmdRet;
-            //                                    }
-            //                                }
-            //                            }
-            //                        }
-
-            //                        // 見つからなければパイロットが乗っているユニットを削除
-            //                        foreach (Unit currentU1 in SRC.UList)
-            //                        {
-            //                            u = currentU1;
-            //                            {
-            //                                var withBlock2 = u.CurrentForm();
-            //                                if ((withBlock2.Name ?? "") == (uname ?? "") && withBlock2.Status != "破棄")
-            //                                {
-            //                                    withBlock2.Escape(opt);
-            //                                    withBlock2.Pilot((object)1).GetOff();
-            //                                    withBlock2.Status = "破棄";
-            //                                    var loopTo3 = withBlock2.CountOtherForm();
-            //                                    for (i = 1; i <= loopTo3; i++)
-            //                                    {
-            //                                        Unit localOtherForm7() { object argIndex1 = (object)i; var ret = withBlock2.OtherForm(argIndex1); return ret; }
-
-            //                                        if (localOtherForm7().Status == "他形態")
-            //                                        {
-            //                                            Unit localOtherForm6() { object argIndex1 = (object)i; var ret = withBlock2.OtherForm(argIndex1); return ret; }
-
-            //                                            localOtherForm6().Status = "破棄";
-            //                                        }
-            //                                    }
-
-            //                                    ExecRemoveUnitCmdRet = LineNum + 1;
-            //                                    return ExecRemoveUnitCmdRet;
-            //                                }
-            //                            }
-            //                        }
-
-            //                        break;
-            //                    }
-
-            //                default:
-            //                    {
-            //                        Event.EventErrorMessage = "RemoveUnitの引数の数が違います";
-            //                        ;
-            //#error Cannot convert ErrorStatementSyntax - see comment for details
-            //                        /* Cannot convert ErrorStatementSyntax, CONVERSION ERROR: Conversion for ErrorStatement not implemented, please report this issue in 'Error(0)' at character 430173
-
-
-            //                        Input:
-            //                                        Error(0)
-
-            //                         */
-            //                        break;
-            //                    }
-            //            }
-
-            //return EventData.NextID;
+            return EventData.NextID;
         }
     }
 }
