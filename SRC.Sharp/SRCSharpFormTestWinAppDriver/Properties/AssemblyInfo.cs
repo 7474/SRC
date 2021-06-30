1d54d6efae2142ae354125332cfe00ccb77111b4
koudenpa <koudenpa@hotmail.com>
2021-07-01 00:15:48 +0900

    Init Appium.WebDriver

---
+++ SRC.Sharp/SRCSharpFormTestWinAppDriver/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SRCSharpFormTestWinAppDriver")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("SRCSharpFormTestWinAppDriver")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0c84a42d-033b-4530-90f9-bf2ff64047be")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
